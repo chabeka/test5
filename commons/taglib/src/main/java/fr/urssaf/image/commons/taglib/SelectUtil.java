1054cb9b82716e7a881393a003dd6489d2fd3db4
bertrand.barault <bertrand.barault>
2010-05-25 16:36:30 +0000

    version 1
    svn path=/java/trunk/; revision=22

---
+++ commons/taglib/src/main/java/fr/urssaf/image/commons/taglib/SelectUtil.java
@@ -0,0 +1,26 @@
+package fr.urssaf.image.commons.taglib;
+
+import java.util.Collection;
+
+public final class SelectUtil {
+	
+	private SelectUtil(){
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	public static boolean contains(Object option, Object value) {
+
+		if (value != null) {
+
+			if (Collection.class.isAssignableFrom(value.getClass())) {
+
+				return ((Collection) value).contains(option);
+			}
+
+			return option.equals(value);
+		}
+
+		return false;
+	}
+}
