62ad500f41b19cfcc1d5e40e06e0e357fcbb8415
bertrand.barault <bertrand.barault>
2010-06-28 15:55:07 +0000

    ajout d'un aspect
    svn path=/java/trunk/; revision=218

---
+++ commons/commons-controller-spring3-exemple/src/main/java/fr/urssaf/image/commons/controller/spring3/exemple/aspect/ControllerAspect.java
@@ -0,0 +1,32 @@
+package fr.urssaf.image.commons.controller.spring3.exemple.aspect;
+
+import java.util.Calendar;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class ControllerAspect {
+
+	private static final Logger log = Logger
+			.getLogger(ControllerAspect.class);
+
+	private static final String TIME = "@annotation(org.springframework.web.bind.annotation.RequestMapping) && execution(* *(..))";
+
+	@Around(TIME)
+	public Object logControllerTime(ProceedingJoinPoint pjp) throws Throwable {
+
+		Calendar calendar1 = Calendar.getInstance();
+
+		Object retVal = pjp.proceed();
+		Calendar calendar2 = Calendar.getInstance();
+
+		log.debug(pjp.getThis().getClass().getSimpleName() + " time:"
+				+ (calendar2.getTimeInMillis() - calendar1.getTimeInMillis())
+				/ 100.0);
+		return retVal;
+	}
+	
+}
