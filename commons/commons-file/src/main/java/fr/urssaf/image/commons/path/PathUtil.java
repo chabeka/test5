b974be2336c0f5b3f01ae681c8df773be84abb26
paul.mareche <paul.mareche>
2010-07-02 12:16:42 +0000

    Ajout d'une classe de fonctions utilitaires pour la manipulation des chemins de fichiers
    svn path=/java/trunk/; revision=242

---
+++ commons/commons-file/src/main/java/fr/urssaf/image/commons/path/PathUtil.java
@@ -0,0 +1,24 @@
+package fr.urssaf.image.commons.path;
+
+import java.io.File;
+
+/**
+ * Bibliothèque de fonctions utilitaires pour manipuler les chemins de fichiers
+ *
+ */
+public class PathUtil {
+
+   
+   /**
+    * Concatène parent et child en utilisant les bons séparateurs de répertoire selon l'OS
+    * @param parent The parent pathname string
+    * @param child The child pathname string
+    * @return le chemin concaténé
+    */
+   public static String combine(String parent, String child)
+   {
+      File file = new File(parent,child);
+      return file.getPath();
+   }
+   
+}
