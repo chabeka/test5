0a29e5832ea4134efee7c8a6736e9313af760e3f
paul.mareche <paul.mareche>
2010-07-08 14:17:49 +0000

    Désactivation des tests sur le service web exemple
    svn path=/java/trunk/; revision=325

--- commons/commons-webservice-wssecurity-spring-client/src/test/java/fr/urssaf/image/commons/webservice/wssecurity/spring/client/service/DocumentServiceTest.java
+++ commons/commons-webservice-wssecurity-spring-client/src/test/java/fr/urssaf/image/commons/webservice/wssecurity/spring/client/service/DocumentServiceTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertEquals;
 import java.rmi.RemoteException;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import fr.urssaf.image.commons.webservice.wssecurity.spring.client.modele.Document;
 
+@Ignore
 public class DocumentServiceTest {
 
 	private DocumentService service = new DocumentServiceImpl();
