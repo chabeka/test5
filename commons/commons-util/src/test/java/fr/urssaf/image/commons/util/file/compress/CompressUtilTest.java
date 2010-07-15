d6de34f214bcd5c17dee17212cb5b89af0e6b904
bertrand.barault <bertrand.barault>
2010-07-15 16:05:26 +0000

    compress : test sur le checksum avec md5
    svn path=/java/trunk/; revision=356

--- commons/commons-util/src/test/java/fr/urssaf/image/commons/util/file/compress/CompressUtilTest.java
+++ commons/commons-util/src/test/java/fr/urssaf/image/commons/util/file/compress/CompressUtilTest.java
@@ -28,11 +28,11 @@ public class CompressUtilTest {
    public void zipDirectory() throws IOException {
 
       long checksum = CompressUtil.zip(archiveFile("dir_zip.zip"),
-            DIRECTORY_TEST,"txt");
-      
-      LOG.debug("zip de "+DIRECTORY_TEST+":" + checksum);
-      assertNotNull("échec du zip "+DIRECTORY_TEST,checksum);
-      
+            DIRECTORY_TEST, "txt");
+
+      LOG.debug("zip de " + DIRECTORY_TEST + ":" + checksum);
+      assertNotNull("échec du zip " + DIRECTORY_TEST, checksum);
+
       LOG.debug(ChecksumFileUtil.md5(archiveFile("dir_zip.zip")));
 
    }
@@ -41,11 +41,9 @@ public class CompressUtilTest {
    public void zipFile() throws IOException {
 
       long checksum = CompressUtil.zip(archiveFile("file_zip.zip"), FILE_TEST);
-      
-      LOG.debug("zip de "+FILE_TEST+":" + checksum);
-      assertNotNull("échec du zip "+FILE_TEST,checksum);
-      
-      
+
+      LOG.debug("zip de " + FILE_TEST + ":" + checksum);
+      assertNotNull("échec du zip " + FILE_TEST, checksum);
 
    }
 
@@ -53,19 +51,19 @@ public class CompressUtilTest {
    public void gzipFile() throws IOException {
 
       long checksum = CompressUtil.gzip(DIRECTORY, FILE_TEST);
-      LOG.debug("gzip de "+FILE_TEST+":" + checksum);
-      
-      assertNotNull("échec du gzip "+FILE_TEST,checksum);
+      LOG.debug("gzip de " + FILE_TEST + ":" + checksum);
+
+      assertNotNull("échec du gzip " + FILE_TEST, checksum);
 
    }
 
    @Test
    public void tgz() throws IOException {
 
-      long checksum = CompressUtil.tgz(DIRECTORY, DIRECTORY_TEST,"txt");
-      LOG.debug("gzip de "+DIRECTORY_TEST+":" + checksum);
-      
-      assertNotNull("échec du gzip "+DIRECTORY_TEST,checksum);
+      long checksum = CompressUtil.tgz(DIRECTORY, DIRECTORY_TEST, "txt");
+      LOG.debug("gzip de " + DIRECTORY_TEST + ":" + checksum);
+
+      assertNotNull("échec du gzip " + DIRECTORY_TEST, checksum);
 
    }
 
@@ -73,9 +71,9 @@ public class CompressUtilTest {
    public void tarFile() throws IOException {
 
       long checksum = CompressUtil.tar(archiveFile("file_tar.tar"), FILE_TEST);
-      LOG.debug("tar de "+FILE_TEST+":" + checksum);
-      
-      assertNotNull("échec du tar "+FILE_TEST,checksum);
+      LOG.debug("tar de " + FILE_TEST + ":" + checksum);
+
+      assertNotNull("échec du tar " + FILE_TEST, checksum);
 
    }
 
@@ -83,12 +81,13 @@ public class CompressUtilTest {
    public void tarDirectory() throws IOException {
 
       long checksum = CompressUtil.tar(archiveFile("dir_tar.tar"),
-            DIRECTORY_TEST,"txt");
-      LOG.debug("tar "+DIRECTORY_TEST+":" + checksum);
-      
-      assertEquals("échec du tar "+DIRECTORY_TEST,"db69deaf8b23a4d8646f8f7b529ccc90",archiveFile("dir_tar.tar"));
-      
+            DIRECTORY_TEST, "txt");
+      LOG.debug("tar " + DIRECTORY_TEST + ":" + checksum);
+
      
+      assertEquals("échec du tar " + DIRECTORY_TEST,
+            "db69deaf8b23a4d8646f8f7b529ccc90", ChecksumFileUtil
+                  .crc32(archiveFile("dir_tar.tar")));
 
    }
 
