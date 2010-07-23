d9b4df9c6ddbf63ad59c55cda96bbd65e462dc68
paul.mareche <paul.mareche>
2010-07-23 06:10:08 +0000

    Base64 : ajout de méthodes et de tests
    svn path=/java/trunk/; revision=390

--- commons/commons-util/src/main/java/fr/urssaf/image/commons/util/base64/Base64Encode.java
+++ commons/commons-util/src/main/java/fr/urssaf/image/commons/util/base64/Base64Encode.java
@@ -9,16 +9,35 @@ import org.apache.commons.codec.binary.Base64InputStream;
 import org.apache.commons.codec.binary.Base64OutputStream;
 import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.lang.SystemUtils;
+import org.apache.log4j.Logger;
 
 
+/**
+ * Encodage en base 64<br><br>
+ * 
+ * Rappels sur la base 64 :<br>
+ * <ul>
+ * <li>base64 est un codage de l'information utilisant 64 caractères, choisis pour être disponibles
+ * sur la majorité des systèmes</li>
+ * <li>un 65ème caractère (signe '=') est utilisé comme complément final</li> 
+ * <li>on transforme 6 bits en 1 caractère simple de l'alphabet base 64 (donc 1 octet)</li>
+ * <li>de ce fait, il faut au minimum écrire 4 octets en base 64 pour coder un message. 
+ * (1er multiple commun entre 6 bits et une représentation en octets (24 bits)</li>
+ * </ul>
+ *
+ */
 public final class Base64Encode {
 
+   private static final Logger LOGGER = Logger.getLogger(Base64Encode.class);
+   
    private Base64Encode() {
 
    }
 
    /**
-    * renvoie le text en base 64 au codage ISO8859_1
+    * Encode une chaîne de caractères en base 64.<br>
+    * Utilise le charset ISO 8859-1 pour traiter la chaîne.
     * 
     * @param text
     *           texte à encoder
@@ -31,11 +50,12 @@ public final class Base64Encode {
    }
 
    /**
-    * renvoie le text en base 64 au codage UTF-8
+    * Encode une chaîne de caractères en base 64.<br>
+    * Utilise le charset UTF-8 pour traiter la chaîne
     * 
     * @param text
     *           texte à encoder
-    * @return chaines de caractères en base 64
+    * @return chaine de caractères en base 64
     */
    public static String encodeUTF8(String text) {
 
@@ -44,42 +64,80 @@ public final class Base64Encode {
    }
 
    /**
-    * renvoie le text en base 64 au codage spécifié par charsetName
+    * Encode une chaîne de caractères en base 64.
     * 
     * @param text
     *           texte à encoder
     * @param charsetName
-    *           codage
-    * @return
+    *           Charset à utiliser pour traiter la chaîne de caractères
+    * @return chaine de caractères en base 64
+    *
     */
    public static String encode(String text, String charsetName) {
 
       byte[] encodage = StringUtils.getBytesUnchecked(text, charsetName);
+      LOGGER.debug("Nombre d'octets représentant la String : " + encodage.length);
       return encode(encodage);
    }
 
+   
    private static String encode(byte[] text) {
       return StringUtils.newStringUtf8(Base64.encodeBase64(text, false));
    }
+   
 
    /**
-    * Encodage d'un fichier en base64
+    * Encodage d'un fichier en base64 et écriture du résultat dans un autre fichier.<br>
+    * Dans le fichier résultat, les données encodées seront séparées sur des lignes
+    * de 76 caractères.<br>
+    * Les lignes se terminent par le séparateur de ligne de l'OS.
     * 
-    * @param decodeFileName
-    *           chemin du fichier décodé
-    * @param encodeFileName
-    *           chemin de sortie du fichier encodé
+    * @param fichierSource
+    *           chemin du fichier à encoder en base 64
+    * @param fichierDest
+    *           chemin du fichier à générer
     * @throws IOException
     *            exception sur les fichiers
     */
-   public static void encodeFile(String decodeFileName, String encodeFileName)
+   public static void encodeFile(String fichierSource, String fichierDest)
+         throws IOException {
+      
+      int longueurLigne = 76; // longueur par défaut
+      byte[] separateurLigne = SystemUtils.LINE_SEPARATOR.getBytes();
+      encodeFile(fichierSource,fichierDest,longueurLigne,separateurLigne);
+      
+   }
+   
+   
+   /**
+    * Encodage d'un fichier en base64 et écriture du résultat dans un autre fichier.<br>
+    * 
+    * @param fichierSource
+    *           chemin du fichier à encoder en base 64
+    * @param fichierDest
+    *           chemin du fichier à générer
+    * @param longueurLigne
+    *           chaque ligne de donnée encodée en base 64 contiendra <i>longueurLigne</i> caractères 
+    *           (arrondi au plus proche multiple de 4). 
+    *           Si longueurLigne<0, les données encodées ne seront pas divisées en lignes.
+    * @param separateurLigne
+    *            chaque ligne de donnée encodée en base 64 se terminera par ces caractères (ex. : \r\n).
+    *            Paramètre ignoré si <i>longueurLigne</i><0
+    * @throws IOException
+    *            exception sur les fichiers
+    */
+   public static void encodeFile(
+         String fichierSource,
+         String fichierDest,
+         int longueurLigne,
+         byte[] separateurLigne)
          throws IOException {
 
-      FileInputStream input = new FileInputStream(decodeFileName);
+      FileInputStream input = new FileInputStream(fichierSource);
 
       Base64OutputStream output = new Base64OutputStream(new FileOutputStream(
-            encodeFileName), true);
-
+            fichierDest), true, longueurLigne, separateurLigne);
+      
       try {
          IOUtils.copy(input, output);
       } finally {
@@ -118,4 +176,6 @@ public final class Base64Encode {
       while (lus>0);
       return base64.toString();
    }
+   
+   
 }
