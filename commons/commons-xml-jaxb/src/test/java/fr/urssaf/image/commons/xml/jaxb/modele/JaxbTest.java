89e3182d89f340058ee5be09ba538eb32b906c4c
bertrand.barault <bertrand.barault>
2010-11-22 08:52:00 +0000

    encodage UTF-8
    svn path=/java/trunk/; revision=512

--- commons/commons-xml-jaxb/src/test/java/fr/urssaf/image/commons/xml/jaxb/modele/JaxbTest.java
+++ commons/commons-xml-jaxb/src/test/java/fr/urssaf/image/commons/xml/jaxb/modele/JaxbTest.java
@@ -21,130 +21,137 @@ import org.junit.Test;
 @SuppressWarnings("PMD")
 public class JaxbTest {
 
-   private static final String REPERTORY;
+	private static final String REPERTORY;
 
-   private static final Date PAST;
+	private static final Date PAST;
 
-   private static final Date FUTURE;
+	private static final Date FUTURE;
 
-   static {
-      REPERTORY = SystemUtils.getJavaIoTmpDir().getAbsolutePath();
+	static {
+		REPERTORY = SystemUtils.getJavaIoTmpDir().getAbsolutePath();
 
-      Calendar past = Calendar.getInstance();
-      past.set(1999, 11, 31);
-      past = DateUtils.truncate(past, Calendar.DATE);
+		Calendar past = Calendar.getInstance();
+		past.set(1999, 11, 31);
+		past = DateUtils.truncate(past, Calendar.DATE);
 
-      PAST = past.getTime();
+		PAST = past.getTime();
 
-      Calendar future = Calendar.getInstance();
-      future.set(2999, 11, 31);
-      future = DateUtils.truncate(future, Calendar.DATE);
+		Calendar future = Calendar.getInstance();
+		future.set(2999, 11, 31);
+		future = DateUtils.truncate(future, Calendar.DATE);
 
-      FUTURE = future.getTime();
+		FUTURE = future.getTime();
 
-   }
+		System.setProperty("file.encoding", "UTF-8");
 
-   private ObjectFactory factory;
+	}
 
-   @Before
-   public void before() {
-      factory = new ObjectFactory();
-   }
+	private ObjectFactory factory;
 
-   private DocumentType createDocument(int id, String titre, Date date) {
-      DocumentType document = factory.createDocumentType();
-      document.id = id;
-      document.titre = titre;
-      document.date = date;
+	@Before
+	public void before() {
+		factory = new ObjectFactory();
+	}
 
-      return document;
-   }
+	private DocumentType createDocument(int id, String titre, Date date) {
+		DocumentType document = factory.createDocumentType();
+		document.id = id;
+		document.titre = titre;
+		document.date = date;
 
-   private AuteurType createAuteur(int id, String nom) {
-      AuteurType auteur = factory.createAuteurType();
-      auteur.id = id;
-      auteur.nom = nom;
+		return document;
+	}
 
-      return auteur;
-   }
+	private AuteurType createAuteur(int id, String nom) {
+		AuteurType auteur = factory.createAuteurType();
+		auteur.id = id;
+		auteur.nom = nom;
 
-   private EtatType createEtat(int id, String libelle, Date date) {
-      EtatType etat = factory.createEtatType();
-      etat.id = id;
-      etat.libelle = libelle;
-      etat.date = date;
+		return auteur;
+	}
 
-      return etat;
-   }
+	private EtatType createEtat(int id, String libelle, Date date) {
+		EtatType etat = factory.createEtatType();
+		etat.id = id;
+		etat.libelle = libelle;
+		etat.date = date;
 
-   @Test
-   public void marshall() throws JAXBException {
+		return etat;
+	}
 
-      Documents docs = factory.createDocuments();
+	@Test
+	public void marshall() throws JAXBException {
 
-      AuteurType auteur0 = this.createAuteur(0, "auteur 0");
-      AuteurType auteur1 = this.createAuteur(1, "auteur 1");
+		Documents docs = factory.createDocuments();
 
-      EtatType etat0 = this.createEtat(0, "open", FUTURE);
-      EtatType etat1 = this.createEtat(1, "close", PAST);
-      EtatType etat2 = this.createEtat(2, "open", FUTURE);
+		AuteurType auteur0 = this.createAuteur(0, "auteur 0é");
+		AuteurType auteur1 = this.createAuteur(1, "auteur 1");
 
-      DocumentType document0 = this.createDocument(0, "titre 0", PAST);
-      DocumentType document1 = this.createDocument(1, "titre 1", FUTURE);
+		EtatType etat0 = this.createEtat(0, "open", FUTURE);
+		EtatType etat1 = this.createEtat(1, "close", PAST);
+		EtatType etat2 = this.createEtat(2, "open", FUTURE);
 
-      document0.setAuteur(auteur0);
-      document1.setAuteur(auteur1);
+		DocumentType document0 = this.createDocument(0, "titre 0", PAST);
+		DocumentType document1 = this.createDocument(1, "titre 1", FUTURE);
 
-      document0.getEtat().add(etat0);
-      document0.getEtat().add(etat1);
-      document1.getEtat().add(etat2);
+		document0.setAuteur(auteur0);
+		document1.setAuteur(auteur1);
 
-      docs.getDocument().add(document0);
-      docs.getDocument().add(document1);
+		document0.getEtat().add(etat0);
+		document0.getEtat().add(etat1);
+		document1.getEtat().add(etat2);
 
-      JAXBContext context = JAXBContext.newInstance(Documents.class);
+		docs.getDocument().add(document0);
+		docs.getDocument().add(document1);
 
-      Marshaller marshaller = context.createMarshaller();
-      marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		JAXBContext context = JAXBContext.newInstance(Documents.class);
 
-      marshaller.marshal(docs, System.out);
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-      File output = new File(FilenameUtils.concat(REPERTORY, "documents.xml"));
+		marshaller.marshal(docs, System.out);
 
-      marshaller.marshal(docs, output);
+		File output = new File(FilenameUtils.concat(REPERTORY, "documents.xml"));
 
-   }
+		marshaller.marshal(docs, output);
 
-   @Test
-   public void unmarshall() throws JAXBException {
+	}
 
-      JAXBContext context = JAXBContext.newInstance(Documents.class);
-      Unmarshaller unmarshaller = context.createUnmarshaller();
+	@Test
+	public void unmarshall() throws JAXBException {
 
-      File input = new File("src/test/resources/documents.xml");
-      Documents docs = (Documents) unmarshaller.unmarshal(input);
+		JAXBContext context = JAXBContext.newInstance(Documents.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
 
-      assertDocument(docs.document.get(0), 0, "titre 0", 0, "auteur 0", PAST);
-      assertEtat(docs.document.get(0).getEtat(), 0, 0, "open", FUTURE);
-      assertEtat(docs.document.get(0).getEtat(), 1, 1, "close", PAST);
+		File input = new File("src/test/resources/documents.xml");
+		Documents docs = (Documents) unmarshaller.unmarshal(input);
 
-      assertDocument(docs.document.get(1), 1, "titre 1", 1, "auteur 1", FUTURE);
-      assertEtat(docs.document.get(1).getEtat(), 0, 2, "open", FUTURE);
-   }
+		assertDocument(docs.document.get(0), 0, "titre éloise", 0, "auteur 0",
+				PAST);
+		assertEtat(docs.document.get(0).getEtat(), 0, 0, "open", FUTURE);
+		assertEtat(docs.document.get(0).getEtat(), 1, 1, "close", PAST);
 
-   private void assertDocument(DocumentType document, int id, String titre,
-         int idAuteur, String nomAuteur, Date date) {
+		assertDocument(docs.document.get(1), 1, "titre 1", 1, "auteur 1",
+				FUTURE);
+		assertEtat(docs.document.get(1).getEtat(), 0, 2, "open", FUTURE);
+	}
 
-      assertEquals(Integer.valueOf(id), document.id);
-      assertEquals(date, document.getDate());
-   }
+	private void assertDocument(DocumentType document, int id, String titre,
+			int idAuteur, String nomAuteur, Date date) {
 
-   private void assertEtat(List<EtatType> etats, int index, int id,
-         String libelle, Date date) {
+		assertEquals(Integer.valueOf(id), document.id);
+		assertEquals(titre, document.titre);
+		assertEquals(Integer.valueOf(idAuteur), document.auteur.id);
+		assertEquals(nomAuteur, document.auteur.nom);
+		assertEquals(date, document.getDate());
+	}
 
-      assertEquals(Integer.valueOf(id), etats.get(index).id);
-      assertEquals(libelle, etats.get(index).libelle);
-      assertEquals(date, etats.get(index).date);
-   }
+	private void assertEtat(List<EtatType> etats, int index, int id,
+			String libelle, Date date) {
+
+		assertEquals(Integer.valueOf(id), etats.get(index).id);
+		assertEquals(libelle, etats.get(index).libelle);
+		assertEquals(date, etats.get(index).date);
+	}
 
 }
