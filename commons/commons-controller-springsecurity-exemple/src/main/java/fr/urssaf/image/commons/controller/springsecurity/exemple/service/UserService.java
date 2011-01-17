c8a8dc8e39f12b6ea80e5fd9cd12d2f9d6c163a6
bertrand.barault <bertrand.barault>
2011-01-17 14:06:55 +0000

    - max de session par utilisateur - correction sur UserService
    svn path=/java/trunk/; revision=655

--- commons/commons-controller-springsecurity-exemple/src/main/java/fr/urssaf/image/commons/controller/springsecurity/exemple/service/UserService.java
+++ commons/commons-controller-springsecurity-exemple/src/main/java/fr/urssaf/image/commons/controller/springsecurity/exemple/service/UserService.java
@@ -1,5 +1,6 @@
 package fr.urssaf.image.commons.controller.springsecurity.exemple.service;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import fr.urssaf.image.commons.controller.springsecurity.exemple.authenticate.SecurityUser;
@@ -10,6 +11,10 @@ public class UserService {
    public SecurityUser findByLoginPasswd(String login, String password) {
 
       SecurityUser user = AuthenticateService.find(login);
+      
+      if (user == null) {
+         throw new UsernameNotFoundException("le compte n'existe pas");
+      }
       
       return user;
    }
