a3f3504eaa9159fe4255843568c37c833df994bb
robert.legrand <robert.legrand>
2011-04-04 12:14:44 +0000

    Après revue de code la classe de manipulation des objets DOM devient JDOMDocument et s'enrichit de méthodes de traitement des XPath.
    svn path=/java/trunk/; revision=786

--- commons/commons-xml/src/test/java/fr/urssaf/image/commons/xml/JDOMDocumentTest.java
+++ commons/commons-xml/src/test/java/fr/urssaf/image/commons/xml/JDOMDocumentTest.java
@@ -20,7 +20,8 @@ import fr.urssaf.image.commons.util.tempfile.TempFileUtils;
  * Teste les utilitaires sur le document JDOM
  * 
  */
-public class JDOMDocumentUtilsTest {
+@SuppressWarnings("PMD.TooManyMethods")
+public class JDOMDocumentTest {
    private static final String FILE_NAME = "src/test/resources/bibliotheque_jdom.xml";
 
    private static final String TEST = "test";
@@ -29,7 +30,7 @@ public class JDOMDocumentUtilsTest {
     */
    @Test
    public void constructeurSimpleTest() {
-      JDOMDocumentUtils dom = new JDOMDocumentUtils();
+      JDOMDocument dom = new JDOMDocument();
 
       dom.setRacine(new Element(TEST));
 
@@ -41,7 +42,7 @@ public class JDOMDocumentUtilsTest {
     * Teste le constructeur qui crée un noeud racine
     */
    public void constructeurAvecRacineTest() {
-      JDOMDocumentUtils dom = new JDOMDocumentUtils(TEST);
+      JDOMDocument dom = new JDOMDocument(TEST);
       assertNotNull("Le DOM est null", dom.getRacine());
       assertEquals("Noeud racine incorrect", dom.getRacine().getName(), TEST);
    }
@@ -65,23 +66,23 @@ public class JDOMDocumentUtilsTest {
     * @throws JDOMException
     */
    @Test
-   public void litEcritTest() throws JDOMException, IOException {
-      JDOMDocumentUtils dom = new JDOMDocumentUtils();
+   public void readWriteTest() throws JDOMException, IOException {
+      JDOMDocument dom = new JDOMDocument();
 
       // On lit le fichier dans les tests
-      dom.lit(FILE_NAME);
+      dom.read(FILE_NAME);
       assertNotNull("lecture du fichier de test" + FILE_NAME + " incorrecte !",
             dom.getDom());
 
       // On l'écrit
       String cheminFichier = getTemporyFileName();
-      dom.sauve(cheminFichier);
+      dom.save(cheminFichier);
 
       try {
          // On commence à charger un XML valide depuis les ressources
-         dom = new JDOMDocumentUtils();
+         dom = new JDOMDocument();
 
-         dom.lit(cheminFichier);
+         dom.read(cheminFichier);
       } finally {
          // On flingue le fichier temporaire dans tous les cas
          File file = new File(cheminFichier);
@@ -101,9 +102,13 @@ public class JDOMDocumentUtilsTest {
       assertEquals("Le nombre de noeuds attendu est 2 (on en trouve " + ((Integer)liste.size()).toString()+")", liste.size(), 2);
    }
    
-   @Test
-   public void createElementTest() throws JDOMException{
-      JDOMDocumentUtils dom = new JDOMDocumentUtils(TEST);
+   /**
+    * Crée une arborescence minima à parcourir avec des XPath
+    * 
+    * @return Le DOM rempli
+    */
+   private JDOMDocument creationArboDOMmini() {
+      JDOMDocument dom = new JDOMDocument(TEST);
       // Création d'un noeud avec un contenu textuel uniquement
       Element monNoeud=dom.createElement(dom.getRacine(), "node", "Contenu Textuel", null);
       
@@ -120,12 +125,19 @@ public class JDOMDocumentUtilsTest {
       attr.put("attribut_3", "valeur 3.3");
   
       dom.createElement(monSousNoeud, "niveau_3", "Texte de niveau 3", attr);
+      return dom;
+   }
+   
+   
+   @Test
+   public void createElementTest() throws JDOMException{
+      JDOMDocument dom = creationArboDOMmini();
       
       //Création du XPath
       XPath query = XPath.newInstance("//niveau_3/@attribut_2");
       String valeur = query.valueOf(dom.getRacine());
       
-      assertEquals("La valeur de la propriété 2 du noeud de niv 3 est fausse", valeur, "valeur 3.2");
+      assertEquals("La valeur de l'attribut 2 du noeud de niv 3 est fausse", valeur, "valeur 3.2");
       
       query=XPath.newInstance("//niveau_3");
       valeur = query.valueOf(dom.getRacine());
@@ -133,4 +145,60 @@ public class JDOMDocumentUtilsTest {
       assertEquals("La valeur textuelle du noeud de niv 3 est fausse", valeur, "Texte de niveau 3");
 
    }
+   
+   @Test
+   public final void selectSingleNodeTest() throws IllegalArgumentException, JDOMException{
+      JDOMDocument dom = creationArboDOMmini();
+      
+      Element elt = dom.selectSingleNode("//niveau_3");
+      
+      String valeur = elt.getAttributeValue("attribut_2");
+      
+      assertEquals("La valeur de l'attribut 2 du noeud niveau_3 est fausse !", valeur, "valeur 3.2");
+   }
+   
+   @Test
+   public final void selectNodesTest() throws IllegalArgumentException, JDOMException{
+      JDOMDocument dom = creationArboDOMmini();
+      
+      HashMap<String,String> attr = new HashMap<String,String>();
+      attr.put("attribut1", "valeur 2_1");
+      attr.put("attribut2", "valeur 2_2");
+      attr.put("attribut3", "valeur 2_3");
+
+      dom.createElement(dom.getRacine(),"node2", "Contenu textuel 2", attr);
+      
+      List<Element> liste = dom.selectNodes("/" + TEST + "/*");
+      
+      assertEquals("La liste des éléments sous la racine n'est pas bonne !", liste.size(), 2);
+      
+      assertEquals("Le premier noeud n'est pas 'node' !",liste.get(0).getName(), "node");
+      assertEquals("Le deuxième noeud n'est pas 'node2'", liste.get(1).getName(), "node2");
+   }
+   
+   @Test( expected=IllegalArgumentException.class)
+   public final void selectNodesArgIllegalTest() throws JDOMException {
+      JDOMDocument dom = creationArboDOMmini();
+      dom.selectNodes(null);
+   }
+   
+   @Test( expected=IllegalArgumentException.class)
+   public final void selectNodesArgIllegal2Test() throws JDOMException {
+      JDOMDocument dom = creationArboDOMmini();
+      dom.selectNodes(null,"//test/*");
+   }
+   
+   @Test( expected=IllegalArgumentException.class)
+   public final void selectSingleNodeArgIllegalTest() throws JDOMException {
+      JDOMDocument dom = creationArboDOMmini();
+      dom.selectSingleNode("");
+   }
+   
+   @Test( expected=IllegalArgumentException.class)
+   public final void selectSingleNodeArgIllegal2Test() throws JDOMException {
+      JDOMDocument dom = creationArboDOMmini();
+      dom.selectSingleNode(null,"//test/*");
+   }
+
+   
 }
