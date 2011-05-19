f08c5b31c3cf7d42a1d76c1fcc7563fab89b6a04
bertrand.barault <bertrand.barault>
2011-05-19 15:32:20 +0000

    vérification dans igcFactory :  - variable JNDI de l'igcConfig.xml existe  - le fichier igcConfig existe
    svn path=/java/trunk/; revision=909

--- sae/sae-webservices/src/main/java/fr/urssaf/image/sae/webservices/security/igc/IgcFactory.java
+++ sae/sae-webservices/src/main/java/fr/urssaf/image/sae/webservices/security/igc/IgcFactory.java
@@ -1,11 +1,13 @@
 package fr.urssaf.image.sae.webservices.security.igc;
 
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.Assert;
 
 import fr.urssaf.image.sae.igc.IgcServiceFactory;
 import fr.urssaf.image.sae.igc.exception.IgcConfigException;
 import fr.urssaf.image.sae.igc.modele.IgcConfig;
 import fr.urssaf.image.sae.igc.service.IgcConfigService;
+import fr.urssaf.image.sae.igc.util.TextUtils;
 
 /**
  * Classe d'instanciation de {@link IgcFactory}
@@ -18,7 +20,13 @@ public final class IgcFactory {
 
    }
 
-   private static final String ICG_CONFIG_ERROR = "Une erreur s'est produite lors du chargement du fichier de configuration IGC";
+   public static final String ICG_CONFIG_ERROR = "Une erreur s'est produite lors du chargement du fichier de configuration IGC";
+
+   @SuppressWarnings("PMD.LongVariable")
+   public static final String IGC_CONFIG_REQUIRED = "Le contenu de la variable JNDI SAE_Fichier_Configuration_IGC n’est pas renseigné, alors que c’est obligatoire";
+
+   @SuppressWarnings("PMD.LongVariable")
+   public static final String IGC_CONFIG_NOTEXIST = "Le fichier de configuration IGC indiqué par la variable JNDI SAE_Fichier_Configuration_IGC est introuvable (${0})";
 
    /**
     * initialisation des répertoires des fichiers AC racine et des CRL à partir
@@ -43,9 +51,17 @@ public final class IgcFactory {
     * &lt;/IgcConfig>
     * </pre>
     * 
-    * Une exception {@link IllegalArgumentException} avec le message
-    * {@value #ICG_CONFIG_ERROR} est levée si une exception
-    * {@link IgcConfigException} est levée
+    * Une exception {@link IllegalArgumentException} avec le message peut-être
+    * levée
+    * <ul>
+    * <li><code>{@value #IGC_CONFIG_REQUIRED}</code>: le fichier igcConfig.xml
+    * doit être renseigné</li>
+    * <li><code>{@value #IGC_CONFIG_NOTEXIST}</code>: le fichier igcConfig.xml
+    * doit exister</li>
+    * <li><code>{@value #ICG_CONFIG_ERROR}</code>: toute autre exception sur le
+    * fichier igcConfig.xml</li>
+    * </ul>
+    * 
     * 
     * @param igcConfigResource
     *           fichier de configuration de l'IGC
@@ -54,11 +70,17 @@ public final class IgcFactory {
     */
    public static IgcConfig createIgcConfig(FileSystemResource igcConfigResource) {
 
+      Assert.hasText(igcConfigResource.getPath(), IGC_CONFIG_REQUIRED);
+
+      Assert.isTrue(igcConfigResource.getFile().isFile(), TextUtils.getMessage(
+            IGC_CONFIG_NOTEXIST, igcConfigResource.getPath()));
+
       IgcConfigService service = IgcServiceFactory.createIgcConfigService();
 
       try {
          return service.loadConfig(igcConfigResource.getPath());
       } catch (IgcConfigException e) {
+
          throw new IllegalArgumentException(ICG_CONFIG_ERROR, e);
       }
    }
--- sae/sae-webservices/src/test/java/fr/urssaf/image/sae/webservices/security/igc/IgcFactoryTest.java
+++ sae/sae-webservices/src/test/java/fr/urssaf/image/sae/webservices/security/igc/IgcFactoryTest.java
@@ -5,12 +5,18 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
+import javax.naming.NamingException;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 
-import fr.urssaf.image.sae.igc.exception.IgcConfigException;
+import fr.urssaf.image.sae.igc.util.TextUtils;
 
 @SuppressWarnings( { "PMD.MethodNamingConventions" })
 public class IgcFactoryTest {
@@ -19,28 +25,81 @@ public class IgcFactoryTest {
 
    private static final Logger LOG = Logger.getLogger(IgcFactoryTest.class);
 
+   private static final String EXPT_EXPECTED = "Exception non attendue";
+
+   private SimpleNamingContextBuilder builder;
+
+   private static final String IGC_CONFIG_JNDI = "java:comp/env/SAE_Fichier_Configuration_IGC";
+
+   @Before
+   public void before() throws NamingException {
+
+      builder = SimpleNamingContextBuilder.emptyActivatedContextBuilder();
+
+   }
+
+   private static ApplicationContext createApplicationContext() {
+
+      return new ClassPathXmlApplicationContext(new String[] {
+            "applicationContext.xml", "applicationContext-security.xml" });
+   }
+
    @Test
    @Ignore
-   public void createIgcConfig_success() throws IOException {
+   public void loadIgcConfig_success() {
 
       fail("non implementé");
    }
 
    @Test
-   public void createIgcConfig_failure() throws IOException {
+   public void loadIgcConfig_failure_igcConfig_required() throws IOException,
+         NamingException {
+
+      // paramétrage de la variable JNDI
+      builder.bind(IGC_CONFIG_JNDI, "");
 
       try {
-
-         FileSystemResource igcConfigResource = new FileSystemResource(
-               "test.xml");
-
-         IgcFactory.createIgcConfig(igcConfigResource);
-
+         createApplicationContext();
          fail(FAIL_MSG);
-      } catch (IllegalArgumentException e) {
-         LOG.debug(e.getCause().getMessage());
-         assertEquals("Exception non attendue", IgcConfigException.class, e
-               .getCause().getClass());
+      } catch (Exception e) {
+
+         Throwable exception = ExceptionUtils.getRootCause(e);
+
+         LOG.debug(exception.getMessage());
+
+         assertEquals(EXPT_EXPECTED, IllegalArgumentException.class, exception
+               .getClass());
+
+         assertEquals(EXPT_EXPECTED, IgcFactory.IGC_CONFIG_REQUIRED, exception
+               .getMessage());
+
+      }
+
+   }
+
+   @Test
+   public void loadIgcConfig_failure_igcConfig_notexist() throws IOException {
+
+      String igcConfig = "notexist.xml";
+
+      builder.bind(IGC_CONFIG_JNDI, igcConfig);
+
+      try {
+         createApplicationContext();
+         fail(FAIL_MSG);
+      } catch (Exception e) {
+
+         Throwable exception = ExceptionUtils.getRootCause(e);
+
+         LOG.debug(exception.getMessage());
+
+         assertEquals(EXPT_EXPECTED, IllegalArgumentException.class, exception
+               .getClass());
+
+         assertEquals(EXPT_EXPECTED, TextUtils.getMessage(
+               IgcFactory.IGC_CONFIG_NOTEXIST, igcConfig), exception
+               .getMessage());
+
       }
    }
 
