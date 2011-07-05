1f85a1dae57dc14bb0837c8a9a4485147d986ae8
lokmen.baadj <lokmen.baadj>
2011-07-05 07:45:15 +0000

    Premier Commit projet SAE ECDE
    svn path=/java/trunk/; revision=965

---
+++ sae/sae-ecde/src/main/java/fr/urssaf/image/sae/ecde/service/validation/EcdeFileServiceValidation.java
@@ -0,0 +1,18 @@
+package fr.urssaf.image.sae.ecde.service.validation;
+
+import org.aspectj.lang.annotation.Aspect;
+
+
+/**
+ * Classe EcdeFileServiceValidation
+ * 
+ * Classe de validation des arguments en entrée des implémentations du service EcdeFileServiceValidation * 
+ *
+ */
+
+@Aspect
+@SuppressWarnings("PMD.AtLeastOneConstructor")
+public class EcdeFileServiceValidation {
+
+   
+}
