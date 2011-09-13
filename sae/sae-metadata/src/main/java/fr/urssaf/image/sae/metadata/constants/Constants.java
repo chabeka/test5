df953e6e4cace6d0c79a404c22aab4c5056c11f3
ahmed.kenore <ahmed.kenore>
2011-09-13 08:16:37 +0000

    Modification : checkRequiredForArchivalMetadata(SAEDocument) checkRequiredForStorageMetadata(SAEDocument) getDefaultConsultableMetadataReferences() getRequiredForArchivalMetadataReferences() getRequiredForStorageMetadataReferences()
    svn path=/java/trunk/; revision=1129

--- sae/sae-metadata/src/main/java/fr/urssaf/image/sae/metadata/constants/Constants.java
+++ sae/sae-metadata/src/main/java/fr/urssaf/image/sae/metadata/constants/Constants.java
@@ -12,20 +12,18 @@ public final class Constants {
 	// Numérique pattern.
 	public static final String DATE_FORMAT = "yyyy-MM-dd";
 	// ici la date est comprise entre 1900-01-01 et 2099-12-31 et doit être de
-	// la syntaxe yyyy/mm/dd
+	// la syntaxe yyyy-mm-dd
 	public static final String DATE_PATTERN = "(19|20)\\d\\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])";
 	// Numérique pattern.
-	public static final String NUMERIC_PATTERN = "^[0-9]*";
+	public static final String NUMERIC_PATTERN = "[0-9]*";
 	// Numérique pattern.
-	public static final String STRING_PATTERN = "^[A-Za-z0-9]*";
+	public static final String STRING_PATTERN = "[A-Za-z0-9]*";
 	// Numérique pattern.
-	public static final String BOOLEAN_PATTERN = "True|true|False||false";
+	public static final String BOOLEAN_PATTERN = "true|false";
 	// le local
 	public static final Locale DEFAULT_LOCAL = Locale.FRENCH;
 	/** encoding de lecture **/
 	public static final Charset ENCODING = Charset.forName("UTF-8");
-	// format de date
-	public static final String DATE_FORMAT_ENTRY = "yyyy/MM/dd";
 	// Message par défaut
 	@SuppressWarnings("PMD.LongVariable")
 	public static final String NO_MESSAGE_FOR_THIS_KEY = "Pas de méssage correspondant à cette clé";
