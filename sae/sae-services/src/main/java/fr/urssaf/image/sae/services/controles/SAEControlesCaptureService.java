a33de42084e18bead881a20e67250a17416265b7
hicham.rhofir <hicham.rhofir>
2011-09-21 09:16:49 +0000

    Ajout : SAECommonCaptureService: 1.	fr.urssaf.image.sae.capture.commons.services.impl.SAECommonCaptureService.buildStorageDocumentForCapture SAEControlesCaptureService: 1.	fr.urssaf.image.sae.capture.services.controles.SAEControlesCaptureService.checkUntypedDocument 2.	fr.urssaf.image.sae.capture.services.controles. SAEControlesCaptureService.checkUntypedMetadata 3.	fr.urssaf.image.sae.capture.services.controles.SAEControlesCaptureService.checkSaeMetadataForCapture 4.	fr.urssaf.image.sae.capture.services.controles.SAEControlesCaptureService.checkSaeMetadataForStorage
    La partie enrichissement  qui correspond à :
    
    RNDRepositoryDAOImpl:
    1.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getAllRndCodes
    2.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getFonctionCodeByRnd
    3.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getActivityCodeByRnd
    4.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getStorageDurationByRnd
    
    SAEEnrichmentMetadataService:
    1.	fr.urssaf.image.sae.capture.services.enrichment.SAEEnrichmentMetadataService.enrichmentMetadata (SAEMetadata)
    
    svn path=/java/trunk/; revision=1173

---
+++ sae/sae-services/src/main/java/fr/urssaf/image/sae/services/controles/SAEControlesCaptureService.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package fr.urssaf.image.sae.services.controles;
+
+import fr.urssaf.image.sae.bo.model.bo.SAEDocument;
+import fr.urssaf.image.sae.bo.model.untyped.UntypedDocument;
+import fr.urssaf.image.sae.exception.capture.DuplicatedMetadataEx;
+import fr.urssaf.image.sae.exception.capture.EmptyDocumentEx;
+import fr.urssaf.image.sae.exception.capture.InvalidValueTypeAndFormatMetadataEx;
+import fr.urssaf.image.sae.exception.capture.NotSpecifiableMetadataEx;
+import fr.urssaf.image.sae.exception.capture.RequiredArchivableMetadataEx;
+import fr.urssaf.image.sae.exception.capture.RequiredStorageMetadataEx;
+import fr.urssaf.image.sae.exception.capture.UnknownHashCodeEx;
+import fr.urssaf.image.sae.exception.capture.UnknownMetadataEx;
+
+/**
+ * Classe de contrôle des métadonnées.
+ * 
+ * @author rhofir.
+ */
+public interface SAEControlesCaptureService {
+
+   /**
+    * Cette méthode permet de vérifier que la taille du contenu est supérieure à
+    * 0 octet.
+    * 
+    * @param untypedDocument
+    *           Classe représentant un document non typé.
+    * @throws EmptyDocumentEx
+    *            {@link EmptyDocumentEx}
+    */
+   void checkUntypedDocument(UntypedDocument untypedDocument)
+         throws EmptyDocumentEx;
+
+   /**
+    * Cette méthode permet de faire les contrôles suivant : <br>
+    * <ul>
+    * <br>
+    * <li>Vérifier l’existence des métadonnées.</li><br>
+    * <li>Vérifier le type/format des métadonnées</li><br>
+    * <li>Vérifier la duplication des métadonnées</li><br>
+    * </ul>
+    * 
+    * @param untypedDocument
+    *           {@link UntypedDocument}
+    * @throws UnknownMetadataEx
+    *            {@link UnknownMetadataEx}
+    * @throws DuplicatedMetadataEx
+    *            {@link DuplicatedMetadataEx}
+    * @throws InvalidValueTypeAndFormatMetadataEx
+    *            {@link InvalidValueTypeAndFormatMetadataEx}
+    */
+   void checkUntypedMetadata(UntypedDocument untypedDocument)
+         throws UnknownMetadataEx, DuplicatedMetadataEx,
+         InvalidValueTypeAndFormatMetadataEx;
+
+   /**
+    * Cette méthode permet de faire les contrôles suivant :<br>
+    * <ul>
+    * <br>
+    * <li>Vérifier lors de l’archivage si les métadonnées spécifiables sont
+    * présentes.</li><br>
+    * <li>Vérifier que l'ensemble des métadonnées obligatoires lors de
+    * l'archivage sont présentes</li><br>
+    * </ul>
+    * 
+    * @param saeDocument
+    *           {@link SAEDocument}
+    * @throws NotSpecifiableMetadataEx
+    *            {@link NotSpecifiableMetadataEx}
+    * @throws RequiredArchivableMetadataEx
+    *            {@link RequiredArchivableMetadataEx}
+    */
+   void checkSaeMetadataForCapture(SAEDocument saeDocument)
+         throws NotSpecifiableMetadataEx, RequiredArchivableMetadataEx;
+
+   /**
+    * Cette méthode permet de vérifier que l'ensemble des métadonnées
+    * obligatoires lors du stockage sont présentes. Cette méthode doit être
+    * appelée après <b>l’enrichissement</b> des métadonnées.
+    * 
+    * @param sAEDocument
+    *           : Classe représentant un document typé de type
+    *           {@link SAEDocument} .
+    * @throws RequiredStorageMetadataEx
+    *            {@link RequiredStorageMetadataEx}
+    */
+   void checkSaeMetadataForStorage(SAEDocument sAEDocument)
+         throws RequiredStorageMetadataEx;
+
+   /**
+    * Cette méthode permet de vérifier la valeur du Hash du document à archiver.
+    * 
+    * @param saeDocument
+    *           : Classe représentant un document typé de type
+    *           {@link SAEDocument} .
+    * @throws UnknownHashCodeEx
+    *            {@link UnknownHashCodeEx}
+    */
+   void checkHashCodeMetadataForStorage(SAEDocument saeDocument)
+         throws UnknownHashCodeEx;
+
+}
