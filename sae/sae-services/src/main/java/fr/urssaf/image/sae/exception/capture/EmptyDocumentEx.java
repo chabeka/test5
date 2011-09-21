445c0ef233f9358ceee2b9e553b5181ce34587d5
hicham.rhofir <hicham.rhofir>
2011-09-21 09:16:28 +0000

    Ajout : SAECommonCaptureService: 1.	fr.urssaf.image.sae.capture.commons.services.impl.SAECommonCaptureService.buildStorageDocumentForCapture SAEControlesCaptureService: 1.	fr.urssaf.image.sae.capture.services.controles.SAEControlesCaptureService.checkUntypedDocument 2.	fr.urssaf.image.sae.capture.services.controles. SAEControlesCaptureService.checkUntypedMetadata 3.	fr.urssaf.image.sae.capture.services.controles.SAEControlesCaptureService.checkSaeMetadataForCapture 4.	fr.urssaf.image.sae.capture.services.controles.SAEControlesCaptureService.checkSaeMetadataForStorage
    La partie enrichissement  qui correspond à :
    
    RNDRepositoryDAOImpl:
    1.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getAllRndCodes
    2.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getFonctionCodeByRnd
    3.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getActivityCodeByRnd
    4.	fr.urssaf.image.sae.capture.services.dao.impl.RNDRepositoryDAO.getStorageDurationByRnd
    
    SAEEnrichmentMetadataService:
    1.	fr.urssaf.image.sae.capture.services.enrichment.SAEEnrichmentMetadataService.enrichmentMetadata (SAEMetadata)
    
    svn path=/java/trunk/; revision=1172

---
+++ sae/sae-services/src/main/java/fr/urssaf/image/sae/exception/capture/EmptyDocumentEx.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package fr.urssaf.image.sae.exception.capture;
+
+
+/**
+ * Une exception est levée si la taille du document est égale à 0 octet.
+ * 
+ * @author rhofir.
+ */
+public class EmptyDocumentEx extends Exception {
+   private static final long serialVersionUID = 1L;
+
+   /**
+    * Construit une nouvelle {@link EmptyDocumentEx} avec un message et
+    * une cause données.
+    * 
+    * @param message
+    *           : Le message d'erreur
+    * @param cause
+    *           : La cause de l'erreur
+    */
+   public EmptyDocumentEx(final String message, final Throwable cause) {
+      super(message, cause);
+   }
+
+   /**
+    * Construit une nouvelle {@link EmptyDocumentEx }avec un message.
+    * 
+    * @param message
+    *           : Le message de l'erreur
+    */
+   public EmptyDocumentEx(final String message) {
+      super(message);
+   }
+
+}
