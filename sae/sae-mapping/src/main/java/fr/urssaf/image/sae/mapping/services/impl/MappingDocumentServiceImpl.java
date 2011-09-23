6d08b83ecf317cfc47d8dfd4e22a5e243f948354
hicham.rhofir <hicham.rhofir>
2011-09-23 13:42:07 +0000

    Modification : Mise à jour des méthodes de conversion.
    svn path=/java/trunk/; revision=1199

--- sae/sae-mapping/src/main/java/fr/urssaf/image/sae/mapping/services/impl/MappingDocumentServiceImpl.java
+++ sae/sae-mapping/src/main/java/fr/urssaf/image/sae/mapping/services/impl/MappingDocumentServiceImpl.java
@@ -33,156 +33,154 @@ import fr.urssaf.image.sae.storage.model.storagedocument.StorageMetadata;
 @Qualifier("mappingDocumentService")
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public final class MappingDocumentServiceImpl implements MappingDocumentService {
-	@Autowired
-	@Qualifier("metadataReferenceDAO")
-	private MetadataReferenceDAO referenceDAO;
+   @Autowired
+   @Qualifier("metadataReferenceDAO")
+   private MetadataReferenceDAO referenceDAO;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@SuppressWarnings({ "PMD.AvoidInstantiatingObjectsInLoops",
-			"PMD.DataflowAnomalyAnalysis" })
-	public StorageDocument saeDocumentToStorageDocument(final SAEDocument saeDoc)
-			throws InvalidSAETypeException {
-		final StorageDocument storageDoc = new StorageDocument();
-		final List<StorageMetadata> sMetadata = new ArrayList<StorageMetadata>();
-		storageDoc.setContent(saeDoc.getContent());
-		storageDoc.setFilePath(saeDoc.getFilePath());
-		for (SAEMetadata metadata : Utils.nullSafeIterable(saeDoc
-				.getMetadatas())) {
-			sMetadata.add(new StorageMetadata(metadata.getShortCode(), metadata
-					.getValue()));
-		}
-		storageDoc.setMetadatas(sMetadata);
+   /**
+    * {@inheritDoc}
+    */
+   @SuppressWarnings( { "PMD.AvoidInstantiatingObjectsInLoops",
+         "PMD.DataflowAnomalyAnalysis" })
+   public StorageDocument saeDocumentToStorageDocument(final SAEDocument saeDoc)
+         throws InvalidSAETypeException {
+      final StorageDocument storageDoc = new StorageDocument();
+      final List<StorageMetadata> sMetadata = new ArrayList<StorageMetadata>();
+      storageDoc.setContent(saeDoc.getContent());
+      storageDoc.setFilePath(saeDoc.getFilePath());
+      for (SAEMetadata metadata : Utils.nullSafeIterable(saeDoc.getMetadatas())) {
+         sMetadata.add(new StorageMetadata(metadata.getShortCode(), metadata
+               .getValue()));
+      }
+      storageDoc.setMetadatas(sMetadata);
 
-		return storageDoc;
-	}
+      return storageDoc;
+   }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 */
-	@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-	public SAEDocument storageDocumentToSaeDocument(
-			final StorageDocument storageDoc) throws InvalidSAETypeException,
-			MappingFromReferentialException {
-		final SAEDocument saeDoc = new SAEDocument();
-		final List<SAEMetadata> metadatas = new ArrayList<SAEMetadata>();
-		saeDoc.setContent(storageDoc.getContent());
-		saeDoc.setFilePath(storageDoc.getFilePath());
-		for (StorageMetadata sMetadata : Utils.nullSafeIterable(storageDoc
-				.getMetadatas())) {
-			try {
-				final MetadataReference reference = referenceDAO
-						.getByShortCode(sMetadata.getShortCode());
-				metadatas.add(new SAEMetadata(reference.getLongCode(),
-						reference.getShortCode(), sMetadata.getValue()));
-			} catch (ReferentialException refExcpt) {
-				throw new MappingFromReferentialException(refExcpt);
-			}
+   /**
+    * {@inheritDoc}
+    * 
+    */
+   @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+   public SAEDocument storageDocumentToSaeDocument(
+         final StorageDocument storageDoc) throws InvalidSAETypeException,
+         MappingFromReferentialException {
+      final SAEDocument saeDoc = new SAEDocument();
+      final List<SAEMetadata> metadatas = new ArrayList<SAEMetadata>();
+      saeDoc.setContent(storageDoc.getContent());
+      saeDoc.setFilePath(storageDoc.getFilePath());
+      for (StorageMetadata sMetadata : Utils.nullSafeIterable(storageDoc
+            .getMetadatas())) {
+         try {
+            final MetadataReference reference = referenceDAO
+                  .getByShortCode(sMetadata.getShortCode());
+            metadatas.add(new SAEMetadata(reference.getLongCode(), reference
+                  .getShortCode(), sMetadata.getValue()));
+         } catch (ReferentialException refExcpt) {
+            throw new MappingFromReferentialException(refExcpt);
+         }
 
-		}
-		saeDoc.setMetadatas(metadatas);
-		return saeDoc;
+      }
+      saeDoc.setMetadatas(metadatas);
+      return saeDoc;
 
-	}
+   }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 */
-	@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-	public UntypedDocument saeDocumentToUntypedDocument(final SAEDocument saeDoc)
-			throws InvalidSAETypeException, MappingFromReferentialException {
-		final List<UntypedMetadata> metadatas = new ArrayList<UntypedMetadata>();
+   /**
+    * {@inheritDoc}
+    * 
+    */
+   @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+   public UntypedDocument saeDocumentToUntypedDocument(final SAEDocument saeDoc)
+         throws InvalidSAETypeException, MappingFromReferentialException {
+      final List<UntypedMetadata> metadatas = new ArrayList<UntypedMetadata>();
 
-		for (SAEMetadata metadata : Utils.nullSafeIterable(saeDoc
-				.getMetadatas())) {
-			try {
-				final MetadataReference reference = referenceDAO
-						.getByLongCode(metadata.getLongCode());
+      for (SAEMetadata metadata : Utils.nullSafeIterable(saeDoc.getMetadatas())) {
+         try {
+            final MetadataReference reference = referenceDAO
+                  .getByLongCode(metadata.getLongCode());
 
-				metadatas.add(new UntypedMetadata(metadata.getLongCode(), Utils
-						.convertToString(metadata.getValue(), reference)));
-			} catch (ParseException parseExcept) {
-				throw new InvalidSAETypeException(parseExcept);
-			} catch (ReferentialException refExcpt) {
-				throw new MappingFromReferentialException(refExcpt);
-			}
-		}
+            metadatas.add(new UntypedMetadata(metadata.getLongCode(), Utils
+                  .convertToString(metadata.getValue(), reference)));
+         } catch (ParseException parseExcept) {
+            throw new InvalidSAETypeException(parseExcept);
+         } catch (ReferentialException refExcpt) {
+            throw new MappingFromReferentialException(refExcpt);
+         }
+      }
 
-		return new UntypedDocument(saeDoc.getFilePath(), saeDoc.getContent(),
-				metadatas);
-	}
+      return new UntypedDocument(saeDoc.getFilePath(), saeDoc.getContent(),
+            metadatas);
+   }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 */
-	@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-	public SAEDocument untypedDocumentToSaeDocument(
-			final UntypedDocument untyped) throws InvalidSAETypeException,
-			MappingFromReferentialException {
-		final List<SAEMetadata> metadatas = new ArrayList<SAEMetadata>();
-		for (UntypedMetadata metadata : Utils.nullSafeIterable(untyped
-				.getUMetadatas())) {
-			try {
-				final MetadataReference reference = referenceDAO
-						.getByLongCode(metadata.getLongCode());
-				metadatas.add(new SAEMetadata(reference.getLongCode(),
-						reference.getShortCode(), Utils.conversionToObject(
-								metadata.getValue(), reference)));
-			} catch (ParseException parseExcept) {
-				throw new InvalidSAETypeException(parseExcept);
-			} catch (ReferentialException refExcpt) {
-				throw new MappingFromReferentialException(refExcpt);
-			}
-		}
+   /**
+    * {@inheritDoc}
+    * 
+    */
+   @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+   public SAEDocument untypedDocumentToSaeDocument(final UntypedDocument untyped)
+         throws InvalidSAETypeException, MappingFromReferentialException {
+      final List<SAEMetadata> metadatas = new ArrayList<SAEMetadata>();
+      for (UntypedMetadata metadata : Utils.nullSafeIterable(untyped
+            .getUMetadatas())) {
+         try {
+            final MetadataReference reference = referenceDAO
+                  .getByLongCode(metadata.getLongCode());
+            metadatas.add(new SAEMetadata(reference.getLongCode(), reference
+                  .getShortCode(), Utils.conversionToObject(
+                  metadata.getValue(), reference)));
+         } catch (ParseException parseExcept) {
+            throw new InvalidSAETypeException(parseExcept);
+         } catch (ReferentialException refExcpt) {
+            throw new MappingFromReferentialException(refExcpt);
+         }
+      }
 
-		return new SAEDocument(untyped.getFilePath(), untyped.getContent(),
-				metadatas);
-	}
+      return new SAEDocument(untyped.getFilePath(), untyped.getContent(),
+            metadatas);
+   }
 
-	/**
-	 * @param referenceDAO
-	 *            : Le service du référentiel
-	 */
-	public void setReferenceDAO(final MetadataReferenceDAO referenceDAO) {
-		this.referenceDAO = referenceDAO;
-	}
+   /**
+    * @param referenceDAO
+    *           : Le service du référentiel
+    */
+   public void setReferenceDAO(final MetadataReferenceDAO referenceDAO) {
+      this.referenceDAO = referenceDAO;
+   }
 
-	/**
-	 * @return Le service du référentiel
-	 */
-	public MetadataReferenceDAO getReferenceDAO() {
-		return referenceDAO;
-	}
+   /**
+    * @return Le service du référentiel
+    */
+   public MetadataReferenceDAO getReferenceDAO() {
+      return referenceDAO;
+   }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 */
-	@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-	public UntypedDocument storageDocumentToUntypedDocument(
-			final StorageDocument storage) throws InvalidSAETypeException,
-			MappingFromReferentialException {
-		final List<UntypedMetadata> metadatas = new ArrayList<UntypedMetadata>();
-		for (StorageMetadata metadata : Utils.nullSafeIterable(storage
-				.getMetadatas())) {
-			try {
-				final MetadataReference reference = referenceDAO
-						.getByShortCode(metadata.getShortCode());
-				metadatas.add(new UntypedMetadata(metadata.getShortCode(),
-						Utils.convertToString(metadata.getValue(), reference)));
-			} catch (ParseException parseExcept) {
-				throw new InvalidSAETypeException(parseExcept);
-			} catch (ReferentialException refExcpt) {
-				throw new MappingFromReferentialException(refExcpt);
-			}
-		}
-
-		return new UntypedDocument(storage.getFilePath(), storage.getContent(),
-				metadatas);
-	}
+   /**
+    * {@inheritDoc}
+    * 
+    */
+   @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+   public UntypedDocument storageDocumentToUntypedDocument(
+         final StorageDocument storage) throws InvalidSAETypeException,
+         MappingFromReferentialException {
+      final List<UntypedMetadata> metadatas = new ArrayList<UntypedMetadata>();
+      for (StorageMetadata metadata : Utils.nullSafeIterable(storage
+            .getMetadatas())) {
+         try {
+            final MetadataReference reference = referenceDAO
+                  .getByShortCode(metadata.getShortCode());
+            metadatas.add(new UntypedMetadata(reference.getLongCode(), Utils
+                  .convertToString(metadata.getValue(), reference)));
+         } catch (ParseException parseExcept) {
+            throw new InvalidSAETypeException(parseExcept);
+         } catch (ReferentialException refExcpt) {
+            throw new MappingFromReferentialException(refExcpt);
+         }
+      }
+      UntypedDocument untypedDocument = new UntypedDocument(storage
+            .getFilePath(), storage.getContent(), metadatas);
+      untypedDocument.setUuid(storage.getUuid());
+      return untypedDocument;
+   }
 
 }
--- sae/sae-mapping/src/main/java/fr/urssaf/image/sae/mapping/utils/Utils.java
+++ sae/sae-mapping/src/main/java/fr/urssaf/image/sae/mapping/utils/Utils.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.Map;
 
 import fr.urssaf.image.sae.bo.model.SAEMetadataType;
+import fr.urssaf.image.sae.bo.model.untyped.UntypedMetadata;
 import fr.urssaf.image.sae.mapping.constants.Constants;
 import fr.urssaf.image.sae.metadata.referential.model.MetadataReference;
 
@@ -19,184 +20,185 @@ import fr.urssaf.image.sae.metadata.referential.model.MetadataReference;
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public final class Utils {
-	/**
-	 * Simplifie l'écriture des boucles foreach quand l'argument peut être
-	 * {@code null}.
-	 * 
-	 * @param <T>
-	 *            le type des éléments
-	 * @param anIterable
-	 *            les éléments à parcourir
-	 * @return les éléments, ou une collection vide si l'argument était null
-	 */
-	@SuppressWarnings("PMD.OnlyOneReturn")
-	public static <T> Iterable<T> nullSafeIterable(final Iterable<T> anIterable) {
-		if (anIterable == null) {
-			return Collections.emptyList();
-		} else {
-			return anIterable;
-		}
-	}
+   /**
+    * Simplifie l'écriture des boucles foreach quand l'argument peut être
+    * {@code null}.
+    * 
+    * @param <T>
+    *           le type des éléments
+    * @param anIterable
+    *           les éléments à parcourir
+    * @return les éléments, ou une collection vide si l'argument était null
+    */
+   @SuppressWarnings("PMD.OnlyOneReturn")
+   public static <T> Iterable<T> nullSafeIterable(final Iterable<T> anIterable) {
+      if (anIterable == null) {
+         return Collections.emptyList();
+      } else {
+         return anIterable;
+      }
+   }
 
-	/**
-	 * Simplifie l'écriture des map
-	 * 
-	 * @param map
-	 *            le type des éléments
-	 * @param <K>
-	 *            : type
-	 * @param <V>
-	 *            : type
-	 * @return les éléments, ou une map vide si l'argument était null
-	 */
-	@SuppressWarnings("PMD.OnlyOneReturn")
-	public static <K, V> Map<K, V> nullSafeMap(final Map<K, V> map) {
-		if (map == null) {
-			return Collections.emptyMap();
-		} else {
-			return map;
-		}
-	}
+   /**
+    * Simplifie l'écriture des map
+    * 
+    * @param map
+    *           le type des éléments
+    * @param <K>
+    *           : type
+    * @param <V>
+    *           : type
+    * @return les éléments, ou une map vide si l'argument était null
+    */
+   @SuppressWarnings("PMD.OnlyOneReturn")
+   public static <K, V> Map<K, V> nullSafeMap(final Map<K, V> map) {
+      if (map == null) {
+         return Collections.emptyMap();
+      } else {
+         return map;
+      }
+   }
 
-	/** Cette classe n'est pas faite pour être instanciée. */
-	private Utils() {
-		assert false;
-	}
+   /** Cette classe n'est pas faite pour être instanciée. */
+   private Utils() {
+      assert false;
+   }
 
-	/**
-	 * Convertie une chaîne en Date
-	 * 
-	 * @param date
-	 *            : La chaîne à convertir.
-	 * @return une date à partir d'une chaîne.
-	 * @throws ParseException
-	 *             Exception lorsque le parsing de la chaîne ne se déroule pas
-	 *             bien.
-	 */
-	@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
-	public static Date stringToDate(final String date) throws ParseException {
-		Date newDate = new Date();
-		if (date != null) {
-			SimpleDateFormat formatter = new SimpleDateFormat(
-					Constants.DATE_PATTERN, Constants.DEFAULT_LOCAL);
-			formatter.setLenient(false);
-			newDate = formatter.parse(date);
-			if (formatter.parse(date, new ParsePosition(0)) == null) {
-				formatter = new SimpleDateFormat(Constants.DATE_PATTERN,
-						Constants.DEFAULT_LOCAL);
-				newDate = formatter.parse(date);
-			}
-		}
-		return newDate;
-	}
+   /**
+    * Convertie une chaîne en Date
+    * 
+    * @param date
+    *           : La chaîne à convertir.
+    * @return une date à partir d'une chaîne.
+    * @throws ParseException
+    *            Exception lorsque le parsing de la chaîne ne se déroule pas
+    *            bien.
+    */
+   @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+   public static Date stringToDate(final String date) throws ParseException {
+      Date newDate = new Date();
+      if (date != null) {
+         SimpleDateFormat formatter = new SimpleDateFormat(
+               Constants.DATE_PATTERN, Constants.DEFAULT_LOCAL);
+         formatter.setLenient(false);
+         newDate = formatter.parse(date);
+         if (formatter.parse(date, new ParsePosition(0)) == null) {
+            formatter = new SimpleDateFormat(Constants.DATE_PATTERN,
+                  Constants.DEFAULT_LOCAL);
+            newDate = formatter.parse(date);
+         }
+      }
+      return newDate;
+   }
 
-	/**
-	 * Convertit une date en chaîne
-	 * 
-	 * @param date
-	 *            : La date à convertir
-	 * @return chaîne à partir d'une date
-	 * @throws ParseException
-	 *             Exception lorsque le parsing de la chaîne ne se déroule pas
-	 *             bien.
-	 */
-	@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
-	public static String dateToString(final Date date) throws ParseException {
-		String newDate = Constants.BLANK;
-		if (date != null) {
-			final SimpleDateFormat formatter = new SimpleDateFormat(
-					Constants.DATE_PATTERN, Constants.DEFAULT_LOCAL);
-			newDate = formatter.format(date);
-		}
-		return newDate;
-	}
+   /**
+    * Convertit une date en chaîne
+    * 
+    * @param date
+    *           : La date à convertir
+    * @return chaîne à partir d'une date
+    * @throws ParseException
+    *            Exception lorsque le parsing de la chaîne ne se déroule pas
+    *            bien.
+    */
+   @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+   public static String dateToString(final Date date) throws ParseException {
+      String newDate = Constants.BLANK;
+      if (date != null) {
+         final SimpleDateFormat formatter = new SimpleDateFormat(
+               Constants.DATE_PATTERN, Constants.DEFAULT_LOCAL);
+         newDate = formatter.format(date);
+      }
+      return newDate;
+   }
 
-	/**
-	 * Permet de convertir un valeur de la métadonnée métier typée en chaîne de
-	 * caractère
-	 * 
-	 * @param metadataValue
-	 *            : la valeur de la métadonnée.
-	 * @return une chaîne de caractère.
-	 * @param reference
-	 *            : La métadonnée du referentiel.
-	 * @throws ParseException
-	 *             Exception levée lorsque la convertion n'aboutie pas
-	 * @throws ParseException
-	 */
-	@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
-	public static String convertToString(final Object metadataValue,
-			final MetadataReference reference) throws ParseException {
-		final SAEMetadataType saeType = typeFinder(reference.getType());
-		String target = null;
-		switch (saeType) {
-		case DATE:
-			target = dateToString((Date) metadataValue);
-			break;
-		case INTEGER:
-		case BOOLEAN:
-		case STRING:
-		default:
-			target = String.valueOf(metadataValue);
-			break;
-		}
-		return target;
-	}
+   /**
+    * Permet de convertir un valeur de la métadonnée métier typée en chaîne de
+    * caractère
+    * 
+    * @param metadataValue
+    *           : la valeur de la métadonnée.
+    * @return une chaîne de caractère.
+    * @param reference
+    *           : La métadonnée du referentiel.
+    * @throws ParseException
+    *            Exception levée lorsque la convertion n'aboutie pas
+    * @throws ParseException
+    */
+   @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+   public static String convertToString(final Object metadataValue,
+         final MetadataReference reference) throws ParseException {
+      final SAEMetadataType saeType = typeFinder(reference.getType());
+      String target = null;
+      switch (saeType) {
+      case DATE:
+         if (!"".equals(metadataValue)) {
+            target = dateToString((Date) metadataValue);
+         }
+         break;
+      case INTEGER:
+      case BOOLEAN:
+      case STRING:
+      default:
+         target = String.valueOf(metadataValue);
+         break;
+      }
+      return target;
+   }
 
-	/**
-	 * Permet de trouver le bon type dans l'enumération des types
-	 * 
-	 * @param type
-	 *            : Le type cherché
-	 * @return Le type métier correspondant au type cherché.
-	 */
-	@SuppressWarnings("PMD.OnlyOneReturn")
-	private static SAEMetadataType typeFinder(final String type) {
-		for (SAEMetadataType saeType : SAEMetadataType.values()) {
-			if (saeType.getType().equalsIgnoreCase(type)) {
-				return saeType;
-			}
-		}
-		return null;
-	}
+   /**
+    * Permet de trouver le bon type dans l'enumération des types
+    * 
+    * @param type
+    *           : Le type cherché
+    * @return Le type métier correspondant au type cherché.
+    */
+   @SuppressWarnings("PMD.OnlyOneReturn")
+   private static SAEMetadataType typeFinder(final String type) {
+      for (SAEMetadataType saeType : SAEMetadataType.values()) {
+         if (saeType.getType().equalsIgnoreCase(type)) {
+            return saeType;
+         }
+      }
+      return null;
+   }
 
+   /**
+    * Permet de convertir les valeur des metadonnées {@link UntypedMetadata} en
+    * objet.
+    * 
+    * @param untypedValue
+    *           : La valeur d'une métadonnée
+    * @param reference
+    *           : La métadonnée cu referentiel.
+    * @return une objet
+    * @throws ParseException
+    *            Exception lever lorsque le parsing de la date n'aboutie pas.
+    */
+   @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+   public static Object conversionToObject(final String untypedValue,
+         final MetadataReference reference) throws ParseException {
+      final SAEMetadataType saeType = typeFinder(reference.getType());
 
-	/**
-	 * Permet de convertir les valeur des metadonnées {@link UntypedMetadata} en
-	 * objet.
-	 * 
-	 * @param untypedValue
-	 *            : La valeur d'une métadonnée
-	 * @param reference
-	 *            : La métadonnée cu referentiel.
-	 * @return une objet
-	 * @throws ParseException
-	 *             Exception lever lorsque le parsing de la date n'aboutie pas.
-	 */
-	@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
-	public static Object conversionToObject(final String untypedValue,
-			final MetadataReference reference) throws ParseException {
-		final SAEMetadataType saeType = typeFinder(reference.getType());
-
-		Object value = null;
-		switch (saeType) {
-		case DATE:
-			value = Utils.stringToDate(untypedValue);
-			break;
-		case INTEGER:
-			value = Integer.valueOf(untypedValue);
-			break;
-		case BOOLEAN:
-			value = Boolean.valueOf(untypedValue);
-			break;
-		case LONG:
-			value = Long.valueOf(untypedValue);
-			break;
-		default:
-			value = untypedValue;
-			break;
-		}
-		return value;
-	}
+      Object value = null;
+      switch (saeType) {
+      case DATE:
+         value = Utils.stringToDate(untypedValue);
+         break;
+      case INTEGER:
+         value = Integer.valueOf(untypedValue);
+         break;
+      case BOOLEAN:
+         value = Boolean.valueOf(untypedValue);
+         break;
+      case LONG:
+         value = Long.valueOf(untypedValue);
+         break;
+      default:
+         value = untypedValue;
+         break;
+      }
+      return value;
+   }
 
 }
