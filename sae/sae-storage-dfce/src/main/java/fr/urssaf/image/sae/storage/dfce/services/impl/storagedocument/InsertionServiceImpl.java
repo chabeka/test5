fd03e00281362f297bea664ddfe56058ed2a91fa
ahmed.kenore <ahmed.kenore>
2011-11-03 15:49:27 +0000

    Modification :  Ajout de composant jmx
    svn path=/java/trunk/; revision=1421

--- sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/services/impl/storagedocument/InsertionServiceImpl.java
+++ sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/services/impl/storagedocument/InsertionServiceImpl.java
@@ -49,31 +49,32 @@ import fr.urssaf.image.sae.storage.services.storagedocument.InsertionService;
 @Qualifier("insertionService")
 @SuppressWarnings("PMD.ExcessiveImports")
 public class InsertionServiceImpl extends AbstractServices implements
-      InsertionService {
-   private static final Logger LOGGER = LoggerFactory
-         .getLogger(InsertionServiceImpl.class);
-   @Autowired
-   @Qualifier("deletionService")
-   private DeletionService deletionService;
-   private int jmxStorageIndex;
+		InsertionService {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(InsertionServiceImpl.class);
+	@Autowired
+	@Qualifier("deletionService")
+	private DeletionService deletionService;
+	private int jmxStorageIndex;
 	private int totalDocument;
 	private JmxIndicator indicator;
-   /**
-    * @return : Le service de suppression
-    */
-   public final DeletionService getDeletionService() {
-      return deletionService;
-   }
 
-   /**
-    * @param deletionService
-    *           : Le service de suppression.
-    */
-   public final void setDeletionService(final DeletionService deletionService) {
-      this.deletionService = deletionService;
-   }
+	/**
+	 * @return : Le service de suppression
+	 */
+	public final DeletionService getDeletionService() {
+		return deletionService;
+	}
 
-   /**
+	/**
+	 * @param deletionService
+	 *            : Le service de suppression.
+	 */
+	public final void setDeletionService(final DeletionService deletionService) {
+		this.deletionService = deletionService;
+	}
+
+	/**
 	 * @param indicator
 	 *            : Les indicateurs de l'archivage de masse.
 	 */
@@ -88,155 +89,169 @@ public class InsertionServiceImpl extends AbstractServices implements
 		return indicator;
 	}
 
-   /**
-    * {@inheritDoc}
-    * 
-    * @throws InsertionServiceEx
-    *            Exception levée lorsque l'insertion ne se déroule pas bien
-    */
-   @Loggable(LogLevel.TRACE)
-   @ServiceChecked
-   @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-   public final BulkInsertionResults bulkInsertStorageDocument(
-         final StorageDocuments storageDocuments, final boolean allOrNothing)
-         throws InsertionServiceEx {
-      final List<StorageDocument> storageDocDone = new ArrayList<StorageDocument>();
-      final List<StorageDocumentOnError> storageDocFailed = new ArrayList<StorageDocumentOnError>();
-      jmxStorageIndex = 0;
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @throws InsertionServiceEx
+	 *             Exception levée lorsque l'insertion ne se déroule pas bien
+	 */
+	@Loggable(LogLevel.TRACE)
+	@ServiceChecked
+	@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+	public final BulkInsertionResults bulkInsertStorageDocument(
+			final StorageDocuments storageDocuments, final boolean allOrNothing)
+			throws InsertionServiceEx {
+		final List<StorageDocument> storageDocDone = new ArrayList<StorageDocument>();
+		final List<StorageDocumentOnError> storageDocFailed = new ArrayList<StorageDocumentOnError>();
+		jmxStorageIndex = 0;
 		totalDocument = 0;
 		if (storageDocuments != null
 				&& storageDocuments.getAllStorageDocuments() != null) {
 			totalDocument = storageDocuments.getAllStorageDocuments().size();
 		}
-		indicator.setJmxCountDocument(totalDocument);
-		indicator.setJmxStorageIndex(jmxStorageIndex);
-		indicator.setJmxTreatmentState(BulkProgress.INSERTION_DOCUMENTS);
-      for (StorageDocument storageDocument : Utils
-            .nullSafeIterable(storageDocuments.getAllStorageDocuments())) {
-         try {
-            storageDocument.setUuid(insertStorageDocument(storageDocument)
-                  .getUuid());
-            storageDocDone.add(storageDocument);
-            jmxStorageIndex++;
+		if (indicator != null) {
+			indicator.setJmxCountDocument(totalDocument);
 			indicator.setJmxStorageIndex(jmxStorageIndex);
-         } catch (InsertionServiceEx insertExcp) {
-            if (storageDocument != null) {
-               StorageDocumentOnError storageDocumentOnError = new StorageDocumentOnError(
-                     storageDocument.getMetadatas(), storageDocument
-                           .getContent(), storageDocument.getFilePath(),
-                     "INSERROR : " + insertExcp.getMessage());
-               storageDocumentOnError.setMessageError("INSERROR");
-               storageDocFailed.add(storageDocumentOnError);
-            }
-            if (allOrNothing) {
-               rollback(storageDocDone);
-               // Les documents "rollbackés" ne doivent pas apparaitre dans
-               // BulkInsertionResults
-               storageDocDone.clear();
-               break;
-            }
-         }
-      }
-      return new BulkInsertionResults(new StorageDocuments(storageDocDone),
-            new StorageDocumentsOnError(storageDocFailed));
-   }
+			indicator.setJmxTreatmentState(BulkProgress.INSERTION_DOCUMENTS);
+		}
+		for (StorageDocument storageDocument : Utils
+				.nullSafeIterable(storageDocuments.getAllStorageDocuments())) {
+			try {
+				storageDocument.setUuid(insertStorageDocument(storageDocument)
+						.getUuid());
+				storageDocDone.add(storageDocument);
+				jmxStorageIndex++;
+				if (indicator != null) {
+					indicator.setJmxStorageIndex(jmxStorageIndex);
+				}
+			} catch (InsertionServiceEx insertExcp) {
+				if (storageDocument != null) {
+					StorageDocumentOnError storageDocumentOnError = new StorageDocumentOnError(
+							storageDocument.getMetadatas(),
+							storageDocument.getContent(),
+							storageDocument.getFilePath(), "INSERROR : "
+									+ insertExcp.getMessage());
+					storageDocumentOnError.setMessageError("INSERROR");
+					storageDocFailed.add(storageDocumentOnError);
+				}
+				if (allOrNothing) {
+					rollback(storageDocDone);
+					// Les documents "rollbackés" ne doivent pas apparaitre dans
+					// BulkInsertionResults
+					storageDocDone.clear();
+					break;
+				}
+			}
+		}
+		return new BulkInsertionResults(new StorageDocuments(storageDocDone),
+				new StorageDocumentsOnError(storageDocFailed));
+	}
 
-   /**
-    * Supprime les documents qui ont déjà été insérés avec succès. Appelée dans
-    * le cadre d'un traitement "tout ou rien" qui serait en erreur.
-    * 
-    * @param storageDocDone
-    * @throws InsertionServiceEx
-    *            Levée si le rollback (donc la suppression) échoue.
-    */
-   @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
-   private void rollback(final List<StorageDocument> storageDocDone)
-         throws InsertionServiceEx {
-	   jmxStorageIndex = 0;
+	/**
+	 * Supprime les documents qui ont déjà été insérés avec succès. Appelée dans
+	 * le cadre d'un traitement "tout ou rien" qui serait en erreur.
+	 * 
+	 * @param storageDocDone
+	 * @throws InsertionServiceEx
+	 *             Levée si le rollback (donc la suppression) échoue.
+	 */
+	@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+	private void rollback(final List<StorageDocument> storageDocDone)
+			throws InsertionServiceEx {
+		jmxStorageIndex = 0;
 		totalDocument = 0;
 		if (storageDocDone != null) {
 			totalDocument = storageDocDone.size();
 		}
-		indicator.setJmxCountDocument(totalDocument);
-		indicator.setJmxStorageIndex(jmxStorageIndex);
-		indicator.setJmxTreatmentState(BulkProgress.DELETION_DOCUMENTS);
-      for (StorageDocument strDocument : Utils.nullSafeIterable(storageDocDone)) {
-         try {
-            deletionService.setDeletionServiceParameter(getDfceService());
-            deletionService.deleteStorageDocument(new UUIDCriteria(strDocument
-                  .getUuid(), null));
-            jmxStorageIndex++;
+		if (indicator != null) {
+			indicator.setJmxCountDocument(totalDocument);
 			indicator.setJmxStorageIndex(jmxStorageIndex);
-         } catch (DeletionServiceEx delSerEx) {
-            // FIXME: lever une exception plus parlante pour l'appelant
-            // (par exemple : AllOrNothingRollbackException).
-            // Pour l'instant on laisse cette exception pour ne pas casser
-            // les
-            // signatures.
+			indicator.setJmxTreatmentState(BulkProgress.DELETION_DOCUMENTS);
+		}
+		for (StorageDocument strDocument : Utils
+				.nullSafeIterable(storageDocDone)) {
+			try {
+				deletionService.setDeletionServiceParameter(getDfceService());
+				deletionService.deleteStorageDocument(new UUIDCriteria(
+						strDocument.getUuid(), null));
+				jmxStorageIndex++;
+				if (indicator != null) {
+					indicator.setJmxStorageIndex(jmxStorageIndex);
+				}
+			} catch (DeletionServiceEx delSerEx) {
+				// FIXME: lever une exception plus parlante pour l'appelant
+				// (par exemple : AllOrNothingRollbackException).
+				// Pour l'instant on laisse cette exception pour ne pas casser
+				// les
+				// signatures.
 
-            // TODO : il faut continuer le rollback sur les autres
-            // documents.
-            // L'appelant pourrait obtenir une liste des documents non
-            // rollbackés via un attribut
-            // de l'exception AllOrNothingRollbackException
-            throw new InsertionServiceEx(StorageMessageHandler
-                  .getMessage(Constants.DEL_CODE_ERROR), delSerEx.getMessage(),
-                  delSerEx);
-         }
-      }
-   }
+				// TODO : il faut continuer le rollback sur les autres
+				// documents.
+				// L'appelant pourrait obtenir une liste des documents non
+				// rollbackés via un attribut
+				// de l'exception AllOrNothingRollbackException
+				throw new InsertionServiceEx(
+						StorageMessageHandler
+								.getMessage(Constants.DEL_CODE_ERROR),
+						delSerEx.getMessage(), delSerEx);
+			}
+		}
+	}
 
-   /**
-    * {@inheritDoc}
-    */
-   @Loggable(LogLevel.TRACE)
-   @ServiceChecked
-   public final StorageDocument insertStorageDocument(
-         final StorageDocument storageDocument) throws InsertionServiceEx {
-      try {
-         // Traces debug - entrée méthode
-         String prefixeTrc = "insertStorageDocument()";
-         LOGGER.debug("{} - Début", prefixeTrc);
-         // Fin des traces debug - entrée méthode
-         Document docDfce = BeanMapper.storageDocumentToDfceDocument(
-               getBaseDFCE(), storageDocument);
-         // ici on récupère le chemin du fichier.
+	/**
+	 * {@inheritDoc}
+	 */
+	@Loggable(LogLevel.TRACE)
+	@ServiceChecked
+	public final StorageDocument insertStorageDocument(
+			final StorageDocument storageDocument) throws InsertionServiceEx {
+		try {
+			// Traces debug - entrée méthode
+			String prefixeTrc = "insertStorageDocument()";
+			LOGGER.debug("{} - Début", prefixeTrc);
+			// Fin des traces debug - entrée méthode
+			Document docDfce = BeanMapper.storageDocumentToDfceDocument(
+					getBaseDFCE(), storageDocument);
+			// ici on récupère le chemin du fichier.
 
-         final InputStream docContent = new ByteArrayInputStream(FileUtils
-               .readFileToByteArray(new File(storageDocument.getFilePath())));
-         final String[] file = BeanMapper.findFileNameAndExtension(
-               storageDocument, StorageTechnicalMetadatas.NOM_FICHIER
-                     .getShortCode().toString());
-         LOGGER.debug("{} - Enrichissement des métadonnées : "
-               + "ajout de la métadonnée NomFichier valeur : {}.{}",
-               new Object[] { prefixeTrc, file[0], file[1] });
-         LOGGER.debug("{} - Début insertion du document dans DFCE", prefixeTrc);
-         docDfce = getDfceService().getStoreService().storeDocument(docDfce,
-               file[0], file[1], docContent);
-         LOGGER.debug("{} - Document inséré dans DFCE (UUID: {})", prefixeTrc,
-               docDfce.getUuid());
-         LOGGER.debug("{} - Fin insertion du document dans DFCE", prefixeTrc);
-         return BeanMapper.dfceDocumentToStorageDocument(docDfce, null,
-               getDfceService(), false);
-      } catch (TagControlException tagCtrlEx) {
-         throw new InsertionServiceEx(StorageMessageHandler
-               .getMessage(Constants.INS_CODE_ERROR), tagCtrlEx.getMessage(),
-               tagCtrlEx);
-      } catch (Exception except) {
-         throw new InsertionServiceEx(StorageMessageHandler
-               .getMessage(Constants.INS_CODE_ERROR), except.getMessage(),
-               except);
-      }
-   }
+			final InputStream docContent = new ByteArrayInputStream(
+					FileUtils.readFileToByteArray(new File(storageDocument
+							.getFilePath())));
+			final String[] file = BeanMapper.findFileNameAndExtension(
+					storageDocument, StorageTechnicalMetadatas.NOM_FICHIER
+							.getShortCode().toString());
+			LOGGER.debug("{} - Enrichissement des métadonnées : "
+					+ "ajout de la métadonnée NomFichier valeur : {}.{}",
+					new Object[] { prefixeTrc, file[0], file[1] });
+			LOGGER.debug("{} - Début insertion du document dans DFCE",
+					prefixeTrc);
+			docDfce = getDfceService().getStoreService().storeDocument(docDfce,
+					file[0], file[1], docContent);
+			LOGGER.debug("{} - Document inséré dans DFCE (UUID: {})",
+					prefixeTrc, docDfce.getUuid());
+			LOGGER.debug("{} - Fin insertion du document dans DFCE", prefixeTrc);
+			return BeanMapper.dfceDocumentToStorageDocument(docDfce, null,
+					getDfceService(), false);
+		} catch (TagControlException tagCtrlEx) {
+			throw new InsertionServiceEx(
+					StorageMessageHandler.getMessage(Constants.INS_CODE_ERROR),
+					tagCtrlEx.getMessage(), tagCtrlEx);
+		} catch (Exception except) {
+			throw new InsertionServiceEx(
+					StorageMessageHandler.getMessage(Constants.INS_CODE_ERROR),
+					except.getMessage(), except);
+		}
+	}
 
-   /**
-    * {@inheritDoc}
-    */
-   public final <T> void setInsertionServiceParameter(final T parameter) {
-      setDfceService((ServiceProvider) parameter);
+	/**
+	 * {@inheritDoc}
+	 */
+	public final <T> void setInsertionServiceParameter(final T parameter) {
+		setDfceService((ServiceProvider) parameter);
 
-   }
-   /**
+	}
+
+	/**
 	 * @return L'indice du document stocké.
 	 */
 	public final int getJmxStorageIndex() {
