3e69c1b3d5140bb52a88e1eea20a00170a62cfce
hicham.rhofir <hicham.rhofir>
2011-11-17 15:16:38 +0000

    Fonction Consultation - Ajout des traces de debug
    svn path=/java/trunk/; revision=1449

--- sae/sae-storage/src/main/java/fr/urssaf/image/sae/storage/model/storagedocument/searchcriteria/UUIDCriteria.java
+++ sae/sae-storage/src/main/java/fr/urssaf/image/sae/storage/model/storagedocument/searchcriteria/UUIDCriteria.java
@@ -3,6 +3,9 @@ package fr.urssaf.image.sae.storage.model.storagedocument.searchcriteria;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
 import fr.urssaf.image.sae.storage.model.storagedocument.StorageMetadata;
 
 /**
@@ -14,41 +17,57 @@ import fr.urssaf.image.sae.storage.model.storagedocument.StorageMetadata;
  * </ul>
  */
 public class UUIDCriteria extends AbstractCriteria {
-	// Attribut
-	private UUID uuid;
+   // Attribut
+   private UUID uuid;
 
-	/**
-	 * Retourne l’identifiant unique universel.
-	 * 
-	 * @return L'identifiant
-	 */
-	public final UUID getUuid() {
-		return uuid;
-	}
+   /**
+    * Retourne l’identifiant unique universel.
+    * 
+    * @return L'identifiant
+    */
+   public final UUID getUuid() {
+      return uuid;
+   }
 
-	/**
-	 * Initialise l’identifiant unique universel
-	 * 
-	 * @param uuid
-	 *            : L'identifiant
-	 */
-	public final void setUuid(final UUID uuid) {
-		this.uuid = uuid;
-	}
+   /**
+    * Initialise l’identifiant unique universel
+    * 
+    * @param uuid
+    *           : L'identifiant
+    */
+   public final void setUuid(final UUID uuid) {
+      this.uuid = uuid;
+   }
 
-	/**
-	 * Construit un {@link UUIDCriteria}
-	 * 
-	 * @param uuid
-	 *            : L'identifiant unique universel.
-	 * @param desiredStorageMetadatas
-	 *            : Les métadonnées de la recherche.
-	 */
-	@SuppressWarnings("PMD.LongVariable")
-	public UUIDCriteria(final UUID uuid,
-			final List<StorageMetadata> desiredStorageMetadatas) {
-		super(desiredStorageMetadatas);
-		this.uuid = uuid;
-	}
+   /**
+    * Construit un {@link UUIDCriteria}
+    * 
+    * @param uuid
+    *           : L'identifiant unique universel.
+    * @param desiredStorageMetadatas
+    *           : Les métadonnées de la recherche.
+    */
+   @SuppressWarnings("PMD.LongVariable")
+   public UUIDCriteria(final UUID uuid,
+         final List<StorageMetadata> desiredStorageMetadatas) {
+      super(desiredStorageMetadatas);
+      this.uuid = uuid;
+   }
 
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public final String toString() {
+      @SuppressWarnings("PMD.LongVariable")
+      final ToStringBuilder toStrBuilder = new ToStringBuilder(this,
+            ToStringStyle.SHORT_PREFIX_STYLE);
+      toStrBuilder.append("uuid",getUuid());
+      if (getDesiredStorageMetadatas() != null) {
+         for (StorageMetadata metadata : getDesiredStorageMetadatas()) {
+            toStrBuilder.append(metadata.toString());
+         }
+      }
+      return toStrBuilder.toString();
+   }
 }
