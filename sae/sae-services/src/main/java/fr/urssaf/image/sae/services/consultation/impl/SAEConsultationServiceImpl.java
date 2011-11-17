ae967833979270f9c8ab98148112710daa631d10
hicham.rhofir <hicham.rhofir>
2011-11-17 15:18:25 +0000

    Fonction Consultation - Ajout des traces de debug
    svn path=/java/trunk/; revision=1450

--- sae/sae-services/src/main/java/fr/urssaf/image/sae/services/consultation/impl/SAEConsultationServiceImpl.java
+++ sae/sae-services/src/main/java/fr/urssaf/image/sae/services/consultation/impl/SAEConsultationServiceImpl.java
@@ -1,10 +1,15 @@
 package fr.urssaf.image.sae.services.consultation.impl;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 import java.util.Map.Entry;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -16,6 +21,7 @@ import fr.urssaf.image.sae.mapping.services.MappingDocumentService;
 import fr.urssaf.image.sae.metadata.exceptions.ReferentialException;
 import fr.urssaf.image.sae.metadata.referential.model.MetadataReference;
 import fr.urssaf.image.sae.metadata.referential.services.MetadataReferenceDAO;
+import fr.urssaf.image.sae.metadata.utils.Utils;
 import fr.urssaf.image.sae.services.consultation.SAEConsultationService;
 import fr.urssaf.image.sae.services.document.impl.AbstractSAEServices;
 import fr.urssaf.image.sae.services.exception.consultation.SAEConsultationServiceException;
@@ -34,7 +40,8 @@ import fr.urssaf.image.sae.storage.model.storagedocument.searchcriteria.UUIDCrit
 @Qualifier("saeConsultationService")
 public class SAEConsultationServiceImpl extends AbstractSAEServices implements
       SAEConsultationService {
-
+   private static final Logger LOG = LoggerFactory
+         .getLogger(SAEConsultationServiceImpl.class);
    private final MetadataReferenceDAO referenceDAO;
 
    private final MappingDocumentService mappingService;
@@ -62,8 +69,12 @@ public class SAEConsultationServiceImpl extends AbstractSAEServices implements
    @Override
    public final UntypedDocument consultation(UUID idArchive)
          throws SAEConsultationServiceException {
-
-     
+      // Traces debug - entrée méthode
+      String prefixeTrc = "consultation()";
+      LOG.debug("{} - Début", prefixeTrc);
+      LOG.debug("{} - UUID envoyé par l'application cliente : {}", prefixeTrc,
+            idArchive);
+      // Fin des traces debug - entrée méthode
       try {
          this.getStorageServiceProvider().openConnexion();
 
@@ -78,7 +89,8 @@ public class SAEConsultationServiceImpl extends AbstractSAEServices implements
                      .getValue().getShortCode()));
 
             }
-
+            LOG.debug("{} - Liste des métadonnées consultable : \"{}\"",
+                  prefixeTrc, buildMessageFromList(metadatas));
             UUIDCriteria uuidCriteria = new UUIDCriteria(idArchive, metadatas);
 
             StorageDocument storageDocument = this.getStorageServiceProvider()
@@ -88,12 +100,11 @@ public class SAEConsultationServiceImpl extends AbstractSAEServices implements
             UntypedDocument untypedDocument = null;
 
             if (storageDocument != null) {
-
                untypedDocument = this.mappingService
                      .storageDocumentToUntypedDocument(storageDocument);
-
             }
-
+            LOG.debug("{} - Sortie", prefixeTrc);
+            // Fin des traces debug - sortie méthode
             return untypedDocument;
 
          } catch (RetrievalServiceEx e) {
@@ -112,7 +123,7 @@ public class SAEConsultationServiceImpl extends AbstractSAEServices implements
 
             throw new SAEConsultationServiceException(e);
 
-         } 
+         }
       } catch (ConnectionServiceEx e) {
 
          throw new SAEConsultationServiceException(e);
@@ -120,4 +131,27 @@ public class SAEConsultationServiceImpl extends AbstractSAEServices implements
 
    }
 
+   /**
+    * Construit une chaîne qui comprends l'ensemble des objets à afficher dans
+    * les logs. <br>
+    * Exemple : "UntypedMetadata[code long:=Titre,value=Attestation],
+    * UntypedMetadata[code long:=DateCreation,value=2011-09-01],
+    * UntypedMetadata[code long:=ApplicationProductrice,value=ADELAIDE]"
+    * 
+    * @param <T>
+    *           le type d'objet
+    * @param list
+    *           : liste des objets à afficher.
+    * @return Une chaîne qui représente l'ensemble des objets à afficher.
+    */
+   private <T> String buildMessageFromList(Collection<T> list) {
+      final ToStringBuilder toStrBuilder = new ToStringBuilder(this,
+            ToStringStyle.SIMPLE_STYLE);
+      for (T o : Utils.nullSafeIterable(list)) {
+         if (o != null) {
+            toStrBuilder.append(o.toString());
+         }
+      }
+      return toStrBuilder.toString();
+   }
 }
