4e21dcaa8552222d92bfbfb6d13e14f0644f0e91
bertrand.barault <bertrand.barault>
2011-11-28 10:20:04 +0000

    jar executable pour le traitement de la capture en masse. il manque la prise en compte la configuration DFCE tirée da la configuration générale du SAE
    svn path=/java/trunk/; revision=1472

---
+++ sae/sae-services-executable/src/main/java/fr/urssaf/image/sae/services/executable/exception/CaptureMasseMainException.java
@@ -0,0 +1,21 @@
+package fr.urssaf.image.sae.services.executable.exception;
+
+/**
+ * Exception levée lors de l’exécution du service d’archivage en masse
+ * 
+ * 
+ */
+public class CaptureMasseMainException extends RuntimeException {
+
+   private static final long serialVersionUID = 1L;
+
+   /**
+    * 
+    * @param cause
+    *           cause de l'exception
+    */
+   public CaptureMasseMainException(Throwable cause) {
+      super(cause);
+   }
+
+}
