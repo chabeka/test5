3e6f2484719b0afbaa9ae5f78a56a04fe6b9facb
bertrand.barault <bertrand.barault>
2011-12-19 13:08:22 +0000

    aucun changement. mise en page
    svn path=/java/trunk/; revision=1580

--- sae/sae-storage/src/main/java/fr/urssaf/image/sae/storage/model/storagedocument/StorageDocumentOnError.java
+++ sae/sae-storage/src/main/java/fr/urssaf/image/sae/storage/model/storagedocument/StorageDocumentOnError.java
@@ -18,80 +18,79 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class StorageDocumentOnError extends AbstractStorageDocument {
 
-	private String codeError;
+   private String codeError;
 
-	private String messageError;
+   private String messageError;
 
-	/**
-	 * Retourne le code erreur
-	 * 
-	 * @return Le code erreur
-	 */
-	public final String getCodeError() {
-		return codeError;
-	}
+   /**
+    * Retourne le code erreur
+    * 
+    * @return Le code erreur
+    */
+   public final String getCodeError() {
+      return codeError;
+   }
 
-	/**
-	 * Initialise le code erreur
-	 * 
-	 * @param codeError
-	 *            : Le code erreur
-	 */
-	public final void setCodeError(final String codeError) {
-		this.codeError = codeError;
-	}
+   /**
+    * Initialise le code erreur
+    * 
+    * @param codeError
+    *           : Le code erreur
+    */
+   public final void setCodeError(final String codeError) {
+      this.codeError = codeError;
+   }
 
-	/**
-	 * Construit un {@link StorageDocumentOnError }.
-	 * 
-	 * @param metadatas
-	 *            : Les metadatas du document
-	 * @param content
-	 *            : Le contenu du document
-	 * @param filePath
-	 *            : Le chemin du document
-	 * @param codeError
-	 *            : Le code Erreur
-	 */
-	public StorageDocumentOnError(final List<StorageMetadata> metadatas,
-			final byte[] content, final String filePath, final String codeError) {
-		super(metadatas, content, filePath);
-		this.codeError = codeError;
-	}
+   /**
+    * Construit un {@link StorageDocumentOnError }.
+    * 
+    * @param metadatas
+    *           : Les metadatas du document
+    * @param content
+    *           : Le contenu du document
+    * @param filePath
+    *           : Le chemin du document
+    * @param codeError
+    *           : Le code Erreur
+    */
+   public StorageDocumentOnError(final List<StorageMetadata> metadatas,
+         final byte[] content, final String filePath, final String codeError) {
+      super(metadatas, content, filePath);
+      this.codeError = codeError;
+   }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public final String toString() {
-		@SuppressWarnings("PMD.LongVariable")
-		final StringBuffer stringBuffer = new StringBuffer();
-		if (getMetadatas() != null) {
-			for (StorageMetadata metadata : getMetadatas()) {
-				stringBuffer.append(metadata.toString());
-			}
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public final String toString() {
+      @SuppressWarnings("PMD.LongVariable")
+      final StringBuffer stringBuffer = new StringBuffer();
+      if (getMetadatas() != null) {
+         for (StorageMetadata metadata : getMetadatas()) {
+            stringBuffer.append(metadata.toString());
+         }
 
-		}
-		return new ToStringBuilder(this).append("content", getContent())
-				.append("codeError", codeError)
-				.append("messageError", messageError)
-				.append("filePath", getFilePath())
-				.append("metadatas", stringBuffer.toString()).toString();
+      }
+      return new ToStringBuilder(this).append("content", getContent()).append(
+            "codeError", codeError).append("messageError", messageError)
+            .append("filePath", getFilePath()).append("metadatas",
+                  stringBuffer.toString()).toString();
 
-	}
+   }
 
-	/**
-	 * @param messageError
-	 *            : Le message d'erreur retourné par l'archivage du document
-	 */
-	public final void setMessageError(final String messageError) {
-		this.messageError = messageError;
-	}
+   /**
+    * @param messageError
+    *           : Le message d'erreur retourné par l'archivage du document
+    */
+   public final void setMessageError(final String messageError) {
+      this.messageError = messageError;
+   }
 
-	/**
-	 * @return Le message d'erreur retourné par l'archivage du document
-	 */
-	public final String getMessageError() {
-		return messageError;
-	}
+   /**
+    * @return Le message d'erreur retourné par l'archivage du document
+    */
+   public final String getMessageError() {
+      return messageError;
+   }
 }
