197401b87d00cdaf1d4e8a52319fe5ac81d1987b
florent.bonnet <florent.bonnet>
2012-01-11 09:07:15 +0000

    ajout de la gestion des points de montage
    svn path=/java/trunk/; revision=1644

--- sae-integration/sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/service/ecde/EcdeService.java
+++ sae-integration/sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/service/ecde/EcdeService.java
@@ -3,6 +3,7 @@ package fr.urssaf.image.sae.integration.ihmweb.service.ecde;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
 import javax.xml.bind.JAXBException;
 
@@ -12,13 +13,13 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
 
-import fr.urssaf.image.sae.integration.ihmweb.config.TestConfig;
 import fr.urssaf.image.sae.integration.ihmweb.exception.IntegrationRuntimeException;
+import fr.urssaf.image.sae.integration.ihmweb.modele.ecde.EcdeSource;
+import fr.urssaf.image.sae.integration.ihmweb.modele.ecde.EcdeSources;
 import fr.urssaf.image.sae.integration.ihmweb.modele.somres.resultats.ResultatsType;
 import fr.urssaf.image.sae.integration.ihmweb.modele.somres.sommaire.SommaireType;
 import fr.urssaf.image.sae.integration.ihmweb.utils.ChecksumUtils;
 import fr.urssaf.image.sae.integration.ihmweb.utils.JAXBUtils;
-
 
 /**
  * Service de manipulation de l'ECDE
@@ -26,48 +27,52 @@ import fr.urssaf.image.sae.integration.ihmweb.utils.JAXBUtils;
 @Service
 public class EcdeService {
 
-   
+   // @Autowired
+   // private TestConfig testConfig;
+
    @Autowired
-   private TestConfig testConfig;
-   
-   
+   private EcdeSources ecdeSources;
+
    /**
     * Convertit une URL ECDE en un chemin de fichier<br>
     * La conversion est faite à LA RACHE
     * 
-    * @param urlEcde l'URL ECDE
+    * @param urlEcde
+    *           l'URL ECDE
     * 
     * @return le chemin de fichier correspondant
     */
    public final String convertUrlEcdeToPath(String urlEcde) {
-      
+
       if (StringUtils.isBlank(urlEcde)) {
-         throw new IntegrationRuntimeException("Impossible de convertir l'URL ECDE en chemin de fichier : l'URL ECDE est vide");
+         throw new IntegrationRuntimeException(
+               "Impossible de convertir l'URL ECDE en chemin de fichier : l'URL ECDE est vide");
       }
 
-//      if (StringUtils.startsWithIgnoreCase(urlEcde, "ecde://ecde.cer69.recouv/")) {
-//         
-//         StringBuffer sBuf = new StringBuffer();
-//         
-//         sBuf.append("Y:\\");
-//         sBuf.append(StringUtils.removeStart(urlEcde, "ecde://ecde.cer69.recouv/"));
-//         
-//         return sBuf.toString();
-//         
-//      } else {
-//         throw new IntegrationRuntimeException("Impossible de convertir l'URL ECDE '" + urlEcde + "' en chemin de fichier.");
-//      }
-      
-      
-      String dnsEcde = testConfig.getDnsEcde();
-      String debutUrlEcde = "ecde://" + dnsEcde + "/";
-      
-      if (StringUtils.startsWithIgnoreCase(urlEcde, debutUrlEcde)) {
+      if (ecdeSources.getSources() == null
+            || ecdeSources.getSources().isEmpty()) {
+         throw new IntegrationRuntimeException(
+               "Aucune correspondance définie dans le fichier");
+      }
+
+      List<EcdeSource> tabEcdeSrc = ecdeSources.getSources();
+      File cheminReseau = null;
+      String debutUrlEcde = null;
+      int i = 0;
+      while (cheminReseau == null && i < tabEcdeSrc.size()) {
+         debutUrlEcde = "ecde://" + tabEcdeSrc.get(i).getHost();
+         if (urlEcde.startsWith(debutUrlEcde)) {
+            cheminReseau = tabEcdeSrc.get(i).getBasePath();
+         }
+         i++;
+      }
+
+      if (cheminReseau != null) {
 
          StringBuffer sBuf = new StringBuffer();
 
-         sBuf.append("Y:\\");
-         sBuf.append(StringUtils.removeStart(urlEcde,debutUrlEcde));
+         sBuf.append(cheminReseau.getAbsolutePath());
+         sBuf.append(StringUtils.removeStart(urlEcde, debutUrlEcde));
 
          return sBuf.toString();
 
@@ -76,47 +81,48 @@ public class EcdeService {
                "Impossible de convertir l'URL ECDE '" + urlEcde
                      + "' en chemin de fichier.");
       }
- 
-      
+
    }
-   
-   
+
    /**
-    * Calcul le SHA-1 du fichier de l'ECDE dont l'URL ECDE est passé en paramètre
+    * Calcul le SHA-1 du fichier de l'ECDE dont l'URL ECDE est passé en
+    * paramètre
     * 
-    * @param urlEcde l'URL ECDE
+    * @param urlEcde
+    *           l'URL ECDE
     * 
     * @return le SHA-1 du fichier, sous la forme d'une chaîne de caractères hexa
     */
    public final String sha1(String urlEcde) {
-      
+
       String cheminFichier = convertUrlEcdeToPath(urlEcde);
-      
+
       return ChecksumUtils.sha1(cheminFichier);
-      
+
    }
-   
-   
+
    /**
     * Charge un fichier resultats.xml et renvoie l'objet le réprésentant
     * 
-    * @param cheminFichierResultats le chemin du fichier resultats.xml
+    * @param cheminFichierResultats
+    *           le chemin du fichier resultats.xml
     * @return l'objet représentant le fichier resultats.xml
     */
    public final ResultatsType chargeResultatsXml(String cheminFichierResultats) {
-      
+
       try {
-      
-         ClassPathResource xsdResultats = new ClassPathResource("xsd_som_res/resultats.xsd");
-         
+
+         ClassPathResource xsdResultats = new ClassPathResource(
+               "xsd_som_res/resultats.xsd");
+
          File xsdFile = xsdResultats.getFile();
-      
+
          Class<ResultatsType> docClass = ResultatsType.class;
-         
+
          FileInputStream input = new FileInputStream(cheminFichierResultats);
-         
+
          return JAXBUtils.unmarshal(docClass, input, xsdFile, null);
-      
+
       } catch (IOException e) {
          throw new IntegrationRuntimeException(e);
       } catch (JAXBException e) {
@@ -125,31 +131,31 @@ public class EcdeService {
          throw new IntegrationRuntimeException(e);
       }
    }
-   
-   
+
    /**
     * Charge un fichier sommaire.xml dans un objet SommaireType
     * 
-    * @param urlEcdeSommaire l'URL ECDE du sommaire
+    * @param urlEcdeSommaire
+    *           l'URL ECDE du sommaire
     * @return l'objet représentant le sommaire.xml
     */
-   public final SommaireType chargeSommaireXml(
-         String urlEcdeSommaire) {
-      
+   public final SommaireType chargeSommaireXml(String urlEcdeSommaire) {
+
       try {
-         
-         ClassPathResource xsdResultats = new ClassPathResource("xsd_som_res/sommaire.xsd");
-         
+
+         ClassPathResource xsdResultats = new ClassPathResource(
+               "xsd_som_res/sommaire.xsd");
+
          File xsdFile = xsdResultats.getFile();
-      
+
          Class<SommaireType> docClass = SommaireType.class;
-         
+
          String cheminFichierSommaire = convertUrlEcdeToPath(urlEcdeSommaire);
-         
+
          FileInputStream input = new FileInputStream(cheminFichierSommaire);
-         
+
          return JAXBUtils.unmarshal(docClass, input, xsdFile, null);
-      
+
       } catch (IOException e) {
          throw new IntegrationRuntimeException(e);
       } catch (JAXBException e) {
@@ -157,7 +163,7 @@ public class EcdeService {
       } catch (SAXException e) {
          throw new IntegrationRuntimeException(e);
       }
-      
+
    }
-   
+
 }
