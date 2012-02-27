2320c2201a0c26b726db73443f0041820a5e8489
samuel.carriere <samuel.carriere>
2012-02-27 10:00:48 +0000

    Suppression de la "dépendance" sur commons-parent ZookeeperMutex rendu réutilisable
    svn path=/java/trunk/; revision=1801

---
+++ src/main/java/fr/urssaf/image/commons/zookeeper/ZookeeperClientFactory.java
@@ -0,0 +1,40 @@
+package fr.urssaf.image.commons.zookeeper;
+
+import java.io.IOException;
+
+import com.netflix.curator.framework.CuratorFramework;
+import com.netflix.curator.framework.CuratorFrameworkFactory;
+import com.netflix.curator.framework.CuratorFrameworkFactory.Builder;
+import com.netflix.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * Factory pour récupérer une connexion à zookeeper
+ *
+ */
+public final class ZookeeperClientFactory {
+
+   private static final int BASE_SLEEP_TIME = 100;    // En milli-secondes
+   private static final int MAX_RETRIES = 10;
+   /**
+    * Constructeur privé
+    */
+   private ZookeeperClientFactory() {
+   }
+
+   /**
+    * Renvoie une connexion à zookeeper
+    * @param connexionString  chaîne de connexion (format : "serveur:port")
+    * @param namespace        namespace de l'application
+    * @return                 connexion
+    * @throws IOException     quand on n'arrive pas à joindre zookeeper
+    */
+   public static CuratorFramework getClient(String connexionString, String namespace) throws IOException {
+      Builder builder = CuratorFrameworkFactory.builder();
+      builder.connectString(connexionString).namespace(namespace);
+      builder.retryPolicy(new ExponentialBackoffRetry(BASE_SLEEP_TIME, MAX_RETRIES));
+      CuratorFramework zkClient = builder.build();
+      zkClient.start();
+      return zkClient;
+   }
+   
+}
