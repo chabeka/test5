96558f28b2c748d68c05a4bb9e0cb10f153e0796
paul.mareche <paul.mareche>
2014-04-29 11:38:49 +0000

    Ajout d'un listener au CuratorFramework pour ne plus avoir la trace "WARN [com.netflix.curator.framework.state.ConnectionStateManager] There are no ConnectionStateListeners registered."
    svn path=/java/trunk/; revision=3995

--- src/main/java/fr/urssaf/image/commons/zookeeper/ZookeeperClientFactory.java
+++ src/main/java/fr/urssaf/image/commons/zookeeper/ZookeeperClientFactory.java
@@ -2,29 +2,39 @@ package fr.urssaf.image.commons.zookeeper;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 
 import com.netflix.curator.framework.CuratorFramework;
 import com.netflix.curator.framework.CuratorFrameworkFactory;
 import com.netflix.curator.framework.CuratorFrameworkFactory.Builder;
+import com.netflix.curator.framework.state.ConnectionState;
+import com.netflix.curator.framework.state.ConnectionStateListener;
 import com.netflix.curator.retry.ExponentialBackoffRetry;
-
 
 /**
  * Factory pour récupérer une connexion à zookeeper
- *
+ * 
  */
 public final class ZookeeperClientFactory implements DisposableBean {
 
-   private static final int BASE_SLEEP_TIME = 100;    // En milli-secondes
+   private static final Logger LOGGER = LoggerFactory
+         .getLogger(ZookeeperClientFactory.class);
+
+   private static final int BASE_SLEEP_TIME = 100; // En milli-secondes
    private static final int MAX_RETRIES = 10;
    private CuratorFramework zkClient;
 
    /**
     * Constructeur
-    * @param zookeeperServer  correspond au serveur zookeeper qu'il faut joindre
-    * @param namespace        namespace de l'application
-    * @throws IOException     quand on n'arrive pas à joindre le serveur zookeeper 
+    * 
+    * @param zookeeperServer
+    *           correspond au serveur zookeeper qu'il faut joindre
+    * @param namespace
+    *           namespace de l'application
+    * @throws IOException
+    *            quand on n'arrive pas à joindre le serveur zookeeper
     */
    public ZookeeperClientFactory(ZookeeperServerBean zookeeperServer,
          String namespace) throws IOException {
@@ -37,24 +47,44 @@ public final class ZookeeperClientFactory implements DisposableBean {
    public CuratorFramework getClient() {
       return zkClient;
    }
-   
+
    /**
-    * Renvoie une connexion à zookeeper.
-    * Attention, il est de la responsabilité de l'appelant de détruire l'objet (appel de
-    * la méthode close) lorsque celui-ci n'est plus utilisé.
-    * @param connexionString  correspond à la chaîne de connexion vers le serveur zookeeper
-    * @param namespace        namespace de l'application
-    * @return                 connexion
-    * @throws IOException     quand on n'arrive pas à joindre zookeeper
+    * Renvoie une connexion à zookeeper. Attention, il est de la responsabilité
+    * de l'appelant de détruire l'objet (appel de la méthode close) lorsque
+    * celui-ci n'est plus utilisé.
+    * 
+    * @param connexionString
+    *           correspond à la chaîne de connexion vers le serveur zookeeper
+    * @param namespace
+    *           namespace de l'application
+    * @return connexion
+    * @throws IOException
+    *            quand on n'arrive pas à joindre zookeeper
     */
    public static CuratorFramework getClient(String connexionString,
          String namespace) throws IOException {
       Builder builder = CuratorFrameworkFactory.builder();
       builder.connectString(connexionString).namespace(namespace);
-      builder.retryPolicy(new ExponentialBackoffRetry(BASE_SLEEP_TIME, MAX_RETRIES));
+      builder.retryPolicy(new ExponentialBackoffRetry(BASE_SLEEP_TIME,
+            MAX_RETRIES));
       CuratorFramework zkClient = builder.build();
+
+      zkClient.getConnectionStateListenable().addListener(
+            new ConnectionStateListener() {
+
+               @Override
+               public void stateChanged(CuratorFramework client,
+                     ConnectionState newState) {
+
+                  LOGGER.debug("Etat connexion: {}", newState.toString());
+
+               }
+            });
+
       zkClient.start();
+
       return zkClient;
+
    }
 
    @Override
@@ -64,5 +94,5 @@ public final class ZookeeperClientFactory implements DisposableBean {
          zkClient = null;
       }
    }
-   
+
 }
