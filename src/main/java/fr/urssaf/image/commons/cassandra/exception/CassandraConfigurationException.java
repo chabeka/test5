da8d23c02b41691714df31d92b54b2f72bb7176a
cedric.lefort <cedric.lefort>
2014-11-13 09:44:14 +0000

    CLE: Commit d'une classe d'exception manquante pour la fonction de transfert
    svn path=/java/trunk/; revision=4125

---
+++ src/main/java/fr/urssaf/image/commons/cassandra/exception/CassandraConfigurationException.java
@@ -0,0 +1,23 @@
+package fr.urssaf.image.commons.cassandra.exception;
+
+
+/**
+ * Exception levée lors d'un problème de configuration de la connection cassandra
+ * 
+ * 
+ */
+public class CassandraConfigurationException extends RuntimeException {
+
+   private static final long serialVersionUID = 1L;
+
+
+   /**
+    * 
+    * @param cause : Exception mère
+    */
+   public CassandraConfigurationException(Throwable cause) {
+      super(cause);
+   }
+
+
+}
